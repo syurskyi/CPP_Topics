ec98f57f8321210c6819bec4d2b672b4198138af
Grey <sergejyurskyj@yahoo.com>
2020-07-06 18:41:00 -0700

    Committed 012_do_While template

--- templates/ITVDN_CPP/CPP_Starter/006_Cyclic/012_do_While/012_do_While.cpp
+++ templates/ITVDN_CPP/CPP_Starter/006_Cyclic/012_do_While/012_do_While.cpp
@@ -1,24 +1,24 @@
-﻿? *s..
-? |i..
-
-u.. s..
-
-in. main()
-{
-	in. count _ 5;
-
-	do
-	{
-		c__ __  "Count " __  count __  e..
-		count--;
-		
-		b..	// Эта инструкция прерывает работу цикла
-
-		c__ __  "This line is never executed";	// Эта строка не выполнится никогда
-	} w___ (count > 0);
-
-	c__ __  "This line is always executed" __  e..
-
-	c__.g..
-	r_ 0;
-}
+﻿//? *s..
+//? |i..
+//
+//u.. s..
+//
+//in. main
+//
+//	in. count _ 5
+//
+//	__
+//
+//		c__ __  "Count " __  ? __  e..
+//		?##
+//
+//		b..	// Эта инструкция прерывает работу цикла
+//
+//		c__ __  "This line is never executed"	// Эта строка не выполнится никогда
+//	 w___ ? > 0
+//
+//	c__ __  "This line is always executed" __  e..
+//
+//	c__.g..
+//	r_ _
+//
