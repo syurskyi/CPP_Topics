3db3ed77a48b218289d93f46caa6dca26318f4c1
Grey <sergejyurskyj@yahoo.com>
2020-07-06 20:08:43 -0700

    Committed 002Function template

--- templates/ITVDN_CPP/CPP_Starter/009_Function_Link/002_Function/002Function.cpp
+++ templates/ITVDN_CPP/CPP_Starter/009_Function_Link/002_Function/002Function.cpp
@@ -1,38 +1,38 @@
-﻿? *s..
-? |i..
-? |s..
-
-u.. s..
-
-v.. Operation()									// 1-я перегрузка.
-v.. Operation(s.. value)						// 2-я перегрузка.
-v.. Operation(s.. value1, in. value2)			// 3-я перегрузка
-v.. Operation(s.. val1, in. val2, d.. val3)	// 4-я перегрузка.
-
-in. main()
-{
-	Operation()                  // 1-я перегрузка.
-	Operation("val")             // 2-я перегрузка.
-	Operation("val", 10)         // 3-я перегрузка. 
-	Operation("val", 10, 12.2)   // 4-я перегрузка.
-
-	sy.. pause
-	r_ 0
-}
-
-v.. Operation()
-{
-	Operation("val", 10, 12.2)
-}
-v.. Operation(s.. value1)
-{
-	Operation(value1, 10, 12.2)
-}
-v.. Operation(s.. value1, in. value2)
-{
-	Operation(value1, value2, 12.2)
-}
-v.. Operation(s.. val1, in. val2, d.. val3)
-{
-	c__ __  val1 __  " " __  val2 __  " " __  val3 __  e..
-}
+﻿//? *s..
+//? |i..
+//? |s..
+//
+//u.. s..
+//
+//v.. ?								// 1-я перегрузка.
+//v.. ? s.. value						// 2-я перегрузка.
+//v.. ? s.. value1, in. value2			// 3-я перегрузка
+//v.. ? s.. val1, in. val2, d.. val3	// 4-я перегрузка.
+//
+//in. main
+//
+//	?                // 1-я перегрузка.
+//	? "val"             // 2-я перегрузка.
+//	? "val", 10       // 3-я перегрузка.
+//	? "val", 10, 12.2   // 4-я перегрузка.
+//
+//	sy.. pause
+//	r_ _
+//
+//
+//v.. Operation
+//
+//	? "val", 10, 12.2
+//
+//v.. Operation s.. value1
+//
+//	? ?, 10, 12.2
+//
+//v.. Operation s.. value1, in. value2
+//
+//	? ? ? 12.2
+//
+//v.. Operation s.. val1, in. val2, d.. val3
+//
+//	c__ __  ? __  " " __  ? __  " " __  ? __  e..
+//
