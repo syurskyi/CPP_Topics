efde3a4a75aa4d5caa71a4180b2e2478b2d8efc8
Grey <sergejyurskyj@yahoo.com>
2020-07-06 18:53:02 -0700

    Committed 007_Functions template

--- templates/ITVDN_CPP/CPP_Starter/007_Inctroduction to function/001_Functions/007_Functions/007_Functions.cpp
+++ templates/ITVDN_CPP/CPP_Starter/007_Inctroduction to function/001_Functions/007_Functions/007_Functions.cpp
@@ -1,34 +1,32 @@
-? *s..
-? |i..
-
-u.. s..
-
-in. AddNumbers(in. a, in. b);
-d.. AddNumbers(d.. a, d.. b);
-
-in. main()
-{
-	in.  value1 _ 2, value2 _ 3;
-
-	//double value3 = 12.3;
-
-	d.. sum _ AddNumbers(value1, value2);
-
-	//double dsum = AddNumbers(12.2, 15.3);
-
-	c__ __  "value1 + value2 = " __  sum __  e..
-
-	sy.. pause
-	r_ 0;
-}
-
-in. AddNumbers(in. a, in. b)
-{
-	c__ __  "Function(int a, int b) is called!" __  e..
-	r_ a + b;
-}
-d.. AddNumbers(d.. a, d.. b)
-{
-	c__ __  "Function(double a, double b) is called!" __  e..
-	r_ a + b;
-}
+//? *s..
+//? |i..
+//
+//u.. s..
+//
+//in. AddNumbers in. a, in. b
+//d.. AddNumbers d.. a, d.. b
+//
+//in. main
+//
+//	in.  value1 _ 2, value2 _ 3
+//
+//	//double value3 = 12.3;
+//
+//	d.. sum _ ? ? ?
+//
+//	//double dsum = AddNumbers(12.2, 15.3);
+//
+//	c__ __  "value1 + value2 = " __  ? __  e..
+//
+//	sy.. pause
+//	r_ _
+//
+//
+//in. AddNumbers in. a, in. b
+//
+//	c__ __  "Function(int a, int b) is called!" __  e..
+//	r_ ? + ?
+//
+//d.. AddNumbers d.. a, d.. b
+//
+//	c__ __  "Function(double a, double b) is called!" __  e..r_ ? + ?
