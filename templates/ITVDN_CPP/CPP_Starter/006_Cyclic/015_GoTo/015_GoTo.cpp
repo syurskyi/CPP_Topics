82b97608928762482653846ff254a28493b8c043
Grey <sergejyurskyj@yahoo.com>
2020-07-06 18:44:06 -0700

    Committed 015_GoTo template

--- templates/ITVDN_CPP/CPP_Starter/006_Cyclic/015_GoTo/015_GoTo.cpp
+++ templates/ITVDN_CPP/CPP_Starter/006_Cyclic/015_GoTo/015_GoTo.cpp
@@ -1,20 +1,20 @@
-? *s..
-? |i..
-
-u.. s..
-
-in. main()
-{
-
-	c__ __  "Start program!" __  e..
-
-	goto label;
-	
-	c__ __  "Some instruction..." __  e..
-
-label: 
-	c__ __  "End program!" __  e..
-
-	c__.g..
-	r_ 0;
-}
+//? *s..
+//? |i..
+//
+//u.. s..
+//
+//in. main
+//
+//
+//	c__ __  "Start program!" __  e..
+//
+//	____ ?
+//
+//	c__ __  "Some instruction..." __  e..
+//
+//label:
+//	c__ __  "End program!" __  e..
+//
+//	c__.g..
+//	r_ _
+//
