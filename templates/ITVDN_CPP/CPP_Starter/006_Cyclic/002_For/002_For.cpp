2e638db99738960c305fe58de055af5677984ba0
Grey <sergejyurskyj@yahoo.com>
2020-07-06 18:30:43 -0700

    Committed 002_For template

--- templates/ITVDN_CPP/CPP_Starter/006_Cyclic/002_For/002_For.cpp
+++ templates/ITVDN_CPP/CPP_Starter/006_Cyclic/002_For/002_For.cpp
@@ -1,17 +1,17 @@
-? *s..
-? |i..
-
-u.. s..
-
-in. main
-
-	in. count _ 0
-
-	___ (in. i _ 5 ? > ? ###
-
-		c__ __  "Count " __  ? __  e..
-
-	c__ __  "This line is always executed";
-
-	c__.g..
-	r_ _
+//? *s..
+//? |i..
+//
+//u.. s..
+//
+//in. main
+//
+//	in. count _ 0
+//
+//	___ (in. i _ 5 ? > ? ###
+//
+//		c__ __  "Count " __  ? __  e..
+//
+//	c__ __  "This line is always executed";
+//
+//	c__.g..
+//	r_ _
