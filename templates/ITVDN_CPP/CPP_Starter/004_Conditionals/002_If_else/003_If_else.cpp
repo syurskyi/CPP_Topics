0075629a369246fbd6c10a28d59db3578adecdf0
Grey <sergejyurskyj@yahoo.com>
2020-07-06 17:58:56 -0700

    Committed 003_If_else template

--- templates/ITVDN_CPP/CPP_Starter/004_Conditionals/002_If_else/003_If_else.cpp
+++ templates/ITVDN_CPP/CPP_Starter/004_Conditionals/002_If_else/003_If_else.cpp
@@ -1,26 +1,26 @@
-? *s..
-? |i..
-
-u.. s..
-
-in. main
-
-    // setlocale(LC_ALL, "rus");
-
-	in. a _ 3, b _ 2
-
-	__ (a < b)
-
-		c__ __  "a < b" __  e..
-
-	____
-
-		c__ __  "a not less than b" __  e..
-
-
-	c__ __  "This line is always executed!" __  e..
-
-	c__.g..
-    r_ _
-
-
+//? *s..
+//? |i..
+//
+//u.. s..
+//
+//in. main
+//
+//    // setlocale(LC_ALL, "rus");
+//
+//	in. a _ 3, b _ 2
+//
+//	__ (a < b)
+//
+//		c__ __  "a < b" __  e..
+//
+//	____
+//
+//		c__ __  "a not less than b" __  e..
+//
+//
+//	c__ __  "This line is always executed!" __  e..
+//
+//	c__.g..
+//    r_ _
+//
+//
--- templates/ITVDN_CPP/CPP_Starter/004_Conditionals/003_If_else/003_If_else.cpp
+++ templates/ITVDN_CPP/CPP_Starter/004_Conditionals/003_If_else/003_If_else.cpp
@@ -1,30 +1,30 @@
-﻿? *s..
-? |i..
-
-u.. s..
-
-in. main()
-{
-    setlocale(LC_ALL, "rus");
-
-	in. a = 2, b = 2;
-
-	__ (a < b)
-	{
-		c__ __  "a < b" __  e..
-	}
-	____ __ (a > b)
-	{
-		c__ __  "a > b" __  e..
-	}
-	____
-	{
-		c__ __  "a = b" __  e..
-	}
-
-	c__ __  "This line is always executed!" __  e..
-
-	c__.g..
-    r_ 0;
-}
-
+﻿//? *s..
+//? |i..
+//
+//u.. s..
+//
+//in. main
+//
+//    setlocale(LC_ALL, "rus");
+//
+//	in. a _ 2, b _ 2
+//
+//	__ (a < b)
+//
+//		c__ __  "a < b" __  e..
+//
+//	____ __ (a > b)
+//
+//		c__ __  "a > b" __  e..
+//
+//	____
+//
+//		c__ __  "a = b" __  e..
+//
+//
+//	c__ __  "This line is always executed!" __  e..
+//
+//	c__.g..
+//    r_ _
+//
+//
