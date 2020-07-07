f3c5dc39736fecdc084d397635f0050d3afffd86
Grey <sergejyurskyj@yahoo.com>
2020-07-07 16:25:43 -0700

    Committed 003Pointer exercise

--- exercises/008_Pointers/005_Pointer/005Pointer.cpp
+++ exercises/008_Pointers/005_Pointer/005Pointer.cpp
@@ -6,13 +6,13 @@
 //in. main
 //
 //	in. age _ 25
-//	in. &? _ *?
+//	in. #? _ *?
 //
 //	c__ __  "Age equals " __  ? __  e..
 //	c__ __  "Address of variable age: 0x" __  ? __  e..
 //
 //	c__ __  "Enter an age: "
-//	c__ __ &?
+//	c__ __ #?
 //
 //	c__ __  "New age equals " __  ? __  e..
 //	c__ __  "New value has 0x" __  ? __  " address" __  e..
