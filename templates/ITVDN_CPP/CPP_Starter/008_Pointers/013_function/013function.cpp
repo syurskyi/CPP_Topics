cf9d55149f91c99a986eaee8019deb1b48cd0c61
Grey <sergejyurskyj@yahoo.com>
2020-07-06 19:56:59 -0700

    Committed 013function template

--- templates/ITVDN_CPP/CPP_Starter/008_Pointers/013_function/013function.cpp
+++ templates/ITVDN_CPP/CPP_Starter/008_Pointers/013_function/013function.cpp
@@ -1,30 +1,29 @@
-﻿? *s..
-? |i..
-
-u.. s..
-
-// Константный указатель на константные данные,т.е. ничто не может быть изменено
-v.. CalcArea(c.. d..* c.. pPi, c.. d..* c.. pRadius, d..* c.. pArea)
-{
-	// проверить указатели перед использованием!
-	__ (pPi && pRadius && pArea)
-		*pArea _ (*pPi) * (*pRadius) * (*pRadius)
- }
-
-in. main()
-{
-	c.. d.. pi _ 3.1416
-	c__ __  "Enter radius of circle: "
-
-	d.. radius _ 0
-	c__ __ radius
-
-	d.. area _ 0
-
-	CalcArea(&pi, &radius, &area)
-
-	c__ __  "Area is = " __  area __  e..
-
-	sy.. pause
-	r_ 0
-}
+﻿//? *s..
+//? |i..
+//
+//u.. s..
+//
+//// Константный указатель на константные данные,т.е. ничто не может быть изменено
+//v.. CalcArea c.. d..& c.. _Pi, c.. d..& c.. _Radius, d..& c.. _Area)
+//
+//	// проверить указатели перед использованием!
+//	__ ? ## ? ## ?
+//		&_A.. _ &_P.. * &_R.. * &_R..
+//
+//
+//in. main
+//
+//	c.. d.. pi _ 3.1416
+//	c__ __  "Enter radius of circle: "
+//
+//	d.. radius _ 0
+//	c__ __ ?
+//
+//	d.. area _ 0
+//
+//	? ? ? ?
+//
+//	c__ __  "Area is = " __  a.. __  e..
+//
+//	sy.. pause
+//	r_ _
