86e2b828f546a6309c5e744c86a5ea7fb1f23ef2
Grey <sergejyurskyj@yahoo.com>
2020-07-06 18:39:23 -0700

    Committed 010_whileExample template

--- templates/ITVDN_CPP/CPP_Starter/006_Cyclic/010_whileExample/010_whileExample.cpp
+++ templates/ITVDN_CPP/CPP_Starter/006_Cyclic/010_whileExample/010_whileExample.cpp
@@ -1,40 +1,40 @@
-﻿? *s..
-? |i..
-? |s..
-
-u.. s..
-
-in. main()
-{
-	
-	setlocale(LC_CTYPE, "rus");
-
-	s.. str _ "Guess the desired color from 5 attempts.";
-
-	in. maxAttempt _ 5;		// Допустимое количество попыток
-	in. attempt _ 0;		// Номер текущей попытки
-	s.. color _ "red";	// Задуманый цвет
-
-	w___ (attempt < maxAttempt) {
-		attempt++;
-		c__ __  str __  " Attempt " __  attempt __  e..
-		s.. value _ "";
-		c__ __ value;
-
-		__ (value __ "")
-		{
-			b..
-		}
-		__ (value !_ color)
-		{
-			str _ "You did not guess";
-			c___
-		}
-
-		c__ __  "Congratulations, you guessed right with " __  attempt __  " attempts!" __  e..
-		b..
-	}
-	
-	system("pause");
-	r_ 0;
-}
+﻿//? *s..
+//? |i..
+//? |s..
+//
+//u.. s..
+//
+//in. main
+//
+//
+//	setlocale(LC_CTYPE, "rus")
+//
+//	s.. str _ "Guess the desired color from 5 attempts."
+//
+//	in. maxAttempt _ 5;	// Допустимое количество попыток
+//	in. attempt _ 0		// Номер текущей попытки
+//	s.. color _ "red"	// Задуманый цвет
+//
+//	w___ a.. < m..
+//		a##
+//		c__ __  s.. __  " Attempt " __  a.. __  e..
+//		s.. value _ ""
+//		c__ __ ?
+//
+//		__ ? __ ""
+//
+//			b..
+//
+//		__ v.. !_ c..
+//
+//			str _ "You did not guess"
+//			c___
+//
+//
+//		c__ __  "Congratulations, you guessed right with " __  a.. __  " attempts!" __  e..
+//		b..
+//	}
+//
+//	sy.. pause
+//	r_ _
+//
