3f85a3366715cc3eb2b92db5a11c770f4b15da0f
Grey <sergejyurskyj@yahoo.com>
2020-07-06 18:40:11 -0700

    Committed 011_do_While template

--- templates/ITVDN_CPP/CPP_Starter/006_Cyclic/011_do_While/011_do_While.cpp
+++ templates/ITVDN_CPP/CPP_Starter/006_Cyclic/011_do_While/011_do_While.cpp
@@ -1,20 +1,20 @@
-? *s..
-? |i..
-
-u.. s..
-
-in. main()
-{
-	in. count _ 5;
-
-	do 
-	{
-		c__ __  "Count " __  count__  e..
-		count--;
-	} w___ (count > 0);
-
-	c__ __  "This line is always executed" __  e..
-
-	c__.g..
-	r_ 0;
-}
+//? *s..
+//? |i..
+//
+//u.. s..
+//
+//in. main
+//
+//	in. count _ 5
+//
+//	__
+//
+//		c__ __  "Count " __  ? __  e..
+//		?##
+//	 w___ ? > 0
+//
+//	c__ __  "This line is always executed" __  e..
+//
+//	c__.g..
+//	r_ _
+//
