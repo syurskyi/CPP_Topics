186b255bb7b85a9f910a2d67b302b908a4a5af41
Grey <sergejyurskyj@yahoo.com>
2020-07-06 18:51:43 -0700

    Committed 006_Functions template

--- templates/ITVDN_CPP/CPP_Starter/007_Inctroduction to function/001_Functions/006_Functions/006_Functions.cpp
+++ templates/ITVDN_CPP/CPP_Starter/007_Inctroduction to function/001_Functions/006_Functions/006_Functions.cpp
@@ -1,24 +1,23 @@
-? *s..
-? |i..
-? |s..
-
-u.. s..
-
-s.. Function(s..);
-
-in. main()
-{
-	s.. sentence _ Function("Vladimir");
-
-	c__ __  sentence __  e..
-
-	sy.. pause
-	r_ 0;
-}
-
-s.. Function(s.. name)
-{
-	s.. hi _ "Hello, " + name + "!";
-
-	r_ hi;
-}
+//? *s..
+//? |i..
+//? |s..
+//
+//u.. s..
+//
+//s.. Function s..
+//
+//in. main
+//
+//	s.. sentence _ F.. "Vladimir"
+//
+//	c__ __  ? __  e..
+//
+//	sy.. pause
+//	r_ 0;
+//
+//
+//s.. Function s.. name
+//
+//	s.. hi _ "Hello, " + ? + "!"
+//
+//	r_ ?
