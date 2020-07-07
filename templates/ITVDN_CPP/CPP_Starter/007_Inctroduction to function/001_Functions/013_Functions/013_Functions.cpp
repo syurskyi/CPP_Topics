5008dbf30f795fa9c7890e450d5eb775ddd360f4
Grey <sergejyurskyj@yahoo.com>
2020-07-06 19:01:01 -0700

    Committed 013_Functions template

--- templates/ITVDN_CPP/CPP_Starter/007_Inctroduction to function/001_Functions/013_Functions/013_Functions.cpp
+++ templates/ITVDN_CPP/CPP_Starter/007_Inctroduction to function/001_Functions/013_Functions/013_Functions.cpp
@@ -1,26 +1,25 @@
-? *s..
-? |i..
-
-u.. s..
-
-v.. CallFunction();
-
-in. test _ 0;
-
-in. main()
-{
-	___ (in. i _ 0; i < 5; i++)
-	{
-		CallFunction();
-	}
-
-	c__ __  "Function called CallFunction invoked " __  test __  " times!";
-
-	c__.g..
-	r_ 0;
-}
-
-v.. CallFunction()
-{
-	test++;
-}
+//? *s..
+//? |i..
+//
+//u.. s..
+//
+//v.. CallFunction
+//
+//in. test _ 0
+//
+//in. main
+//
+//	___ in. i _ 0 ? < 5 ?##
+//
+//		?
+//
+//
+//	c__ __  "Function called CallFunction invoked " __  t.. __  " times!";
+//
+//	c__.g..
+//	r_ _
+//
+//
+//v.. CallFunction
+//
+//	t##
