e9ed9acb81e06b8167434f975675336e8e0aceb2
Grey <sergejyurskyj@yahoo.com>
2020-07-06 20:11:14 -0700

    Committed 004Recursion template

--- templates/ITVDN_CPP/CPP_Starter/009_Function_Link/004_Recursion/004Recursion.cpp
+++ templates/ITVDN_CPP/CPP_Starter/009_Function_Link/004_Recursion/004Recursion.cpp
@@ -1,27 +1,26 @@
-﻿? *s..
-? |i..
-
-u.. s..
-
-v.. recursion(in. counter)sy.. pause
-
-in. main()
-{
-	recursion(3)sy.. pause
-
-	sy.. pause
-	r_ 0sy.. pause
-}
-
-v.. recursion(in. counter)
-{
-	counter--sy.. pause
-
-	c__ __  "First part of recursion function " __  counter __  e..
-
-	__ (counter !_ 0)
-	{
-		recursion(counter)sy.. pause
-	}
-	c__ __  "Second part of recursion function " __  counter __  e..
-}
+﻿//? *s..
+//? |i..
+//
+//u.. s..
+//
+//v.. ? in. counter sy.. pause
+//
+//in. main
+//
+//	? 3 sy.. pause
+//
+//	sy.. pause
+//	r_ 0 sy.. pause
+//
+//
+//v.. recursion in. counter
+//
+//	?## sy.. pause
+//
+//	c__ __  "First part of recursion function " __  ? __  e..
+//
+//	__ ? !_ 0
+//
+//		? ? sy.. pause
+//
+//	c__ __  "Second part of recursion function " __  ? __  e..
