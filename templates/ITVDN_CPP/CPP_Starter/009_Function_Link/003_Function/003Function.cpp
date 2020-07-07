305cad51c2979e8b81840c6bff303bd4429ac4a1
Grey <sergejyurskyj@yahoo.com>
2020-07-06 20:09:55 -0700

    Committed 003Function template

--- templates/ITVDN_CPP/CPP_Starter/009_Function_Link/003_Function/003Function.cpp
+++ templates/ITVDN_CPP/CPP_Starter/009_Function_Link/003_Function/003Function.cpp
@@ -1,23 +1,23 @@
-﻿? *s..
-? |i..
-? |s..
-
-u.. s..
-
-v.. Operation(s.. val1 _ "val", in. val2 _ 10, d.. val3 _ 12.2)sy.. pause
-
-in. main()
-{
-	Operation()sy.. pause                  // 1-я перегрузка.
-	Operation("val")sy.. pause             // 2-я перегрузка.
-	Operation("val", 10)sy.. pause         // 3-я перегрузка.
-	Operation("val", 10, 12.2)sy.. pause   // 4-я перегрузка.
-
-
-	sy.. pause
-	r_ 0sy.. pause
-}
-v.. Operation(s.. val1, in. val2, d.. val3)
-{
-	c__ __  val1 __  " " __  val2 __  " " __  val3 __  e..
-}
+﻿//? *s..
+//? |i..
+//? |s..
+//
+//u.. s..
+//
+//v.. ? s.. val1 _ "val", in. val2 _ 10, d.. val3 _ 12.2| sy.. pause
+//
+//in. main
+//
+//	? sy.. pause                  // 1-я перегрузка.
+//	? "val" sy.. pause             // 2-я перегрузка.
+//	? "val", 10 sy.. pause         // 3-я перегрузка.
+//	? "val", 10, 12.2 sy.. pause   // 4-я перегрузка.
+//
+//
+//	sy.. pause
+//	r_ _ sy.. pause
+//
+//v.. Operation s.. val1, in. val2, d.. val3
+//
+//	c__ __  ? __  " " __  ? __  " " __  ? __  e..
+//
