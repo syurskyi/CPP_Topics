2a822ac4206a7e341d25283d7304937f74a0948f
Grey <sergejyurskyj@yahoo.com>
2020-07-06 18:36:31 -0700

    Committed 007_while template

--- templates/ITVDN_CPP/CPP_Starter/006_Cyclic/007_while/007_while.cpp
+++ templates/ITVDN_CPP/CPP_Starter/006_Cyclic/007_while/007_while.cpp
@@ -1,22 +1,21 @@
-
-? *s..
-? |i..
-
-u.. s..
-
-in. main()
-{
-	in. count _ 5;
-
-
-	w___ (count > 0)
-	{
-		c__ __  "Count = "__  count __  e..
-		count--;
-	}
-
-	c__ __  "This line is always executed";
-
-	c__.g..
-	r_ 0;
-}
+//
+//? *s..
+//? |i..
+//
+//u.. s..
+//
+//in. main
+//
+//	in. count _ 5
+//
+//
+//	w___ ? > 0
+//
+//		c__ __  "Count = "__  ? __  e..
+//		?##
+//
+//
+//	c__ __  "This line is always executed"
+//
+//	c__.g..
+//	r_ _
