2ef8280feebe0a344d5d32cb9c699b0a56e043a7
Grey <sergejyurskyj@yahoo.com>
2020-07-06 18:43:16 -0700

    Committed 014_do_While_Example template

--- templates/ITVDN_CPP/CPP_Starter/006_Cyclic/014_do_While_Example/014_do_While_Example.cpp
+++ templates/ITVDN_CPP/CPP_Starter/006_Cyclic/014_do_While_Example/014_do_While_Example.cpp
@@ -1,34 +1,34 @@
-﻿? *s..
-? |i..
-
-u.. s..
-
-in. main()
-{
-	setlocale(LC_CTYPE, "rus");
-
-	in. number _ 0;		// исло, факториал которого необходимо получить
-	in. factorial _ 1;	// акториал
-
-	c__ __  "Enter number: ";
-	c__ __ number;
-
-	c__ __  "factorial of a number: " __  number __  "! = ";
-
-	do
-	{
-		__ (number __ 0)
-		{
-			factorial _ 1;
-			b..
-		}
-		factorial _ factorial * number;
-		number--;
-
-	} w___ (number > 0);
-	
-	c__ __  factorial __  e..
-	
-	system("pause");
-	r_ 0;
-}
+﻿//? *s..
+//? |i..
+//
+//u.. s..
+//
+//in. main
+//
+//	setlocale(LC_CTYPE, "rus")
+//
+//	in. number _ 0;	// исло, факториал которого необходимо получить
+//	in. factorial _ 1	// акториал
+//
+//	c__ __  "Enter number: "
+//	c__ __ n..
+//
+//	c__ __  "factorial of a number: " __  n.. __  "! = "
+//
+//	__
+//
+//		__ n.. __ 0
+//
+//			f.. _ 1
+//			b..
+//
+//		f.. _ f.. * n..
+//		n..
+//
+//	 w___ n.. > 0
+//
+//	c__ __  f.. __  e..
+//
+//	sy.. pause
+//	r_ _
+//
