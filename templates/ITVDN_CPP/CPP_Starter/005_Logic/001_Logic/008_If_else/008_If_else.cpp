4b4c1efc744e0c25d1d6038e1051227e1dadee9e
Grey <sergejyurskyj@yahoo.com>
2020-07-06 18:18:00 -0700

    Committed 008_If_else template

--- templates/ITVDN_CPP/CPP_Starter/005_Logic/001_Logic/008_If_else/008_If_else.cpp
+++ templates/ITVDN_CPP/CPP_Starter/005_Logic/001_Logic/008_If_else/008_If_else.cpp
@@ -1,31 +1,31 @@
-? *s..
-? |i..
-? |s..
-
-u.. s..
-
-in. main()
-{
-	s.. login _ "Admin";
-	s.. password _ "12345";
-
-	s.. userLogin _ "";
-	s.. userPassword _ "";
-
-	c__ __  "Enter your login: ";
-	c__ __ userLogin;
-
-	c__ __  "Enter the password: ";
-	c__ __ userPassword;
-
-	__ (userLogin __ login && userPassword __ password) {
-		c__ __  "Hello, Admin!" __  e..
-	}
-	____
-	{
-		c__ __  "Hello, User!" __  e..
-	}
-
-	system("pause");
-	r_ 0;
-}
+//? *s..
+//? |i..
+//? |s..
+//
+//u.. s..
+//
+//in. main
+//
+//	s.. login _ "Admin"
+//	s.. password _ "12345"
+//
+//	s.. userLogin _ ""
+//	s.. userPassword _ ""
+//
+//	c__ __  "Enter your login: "
+//	c__ __ uL..
+//
+//	c__ __  "Enter the password: "
+//	c__ __ ?P..
+//
+//	__ (?L.. __ l.. ## ?P.. __ p..
+//		c__ __  "Hello, Admin!" __  e..
+//
+//	____
+//
+//		c__ __  "Hello, User!" __  e..
+//
+//
+//	sy.. pause
+//	r_ _
+//}
