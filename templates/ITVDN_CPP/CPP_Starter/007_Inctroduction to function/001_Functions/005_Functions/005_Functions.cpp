eab67195ef1739861d2fd2f13f3fc99009bfb2e8
Grey <sergejyurskyj@yahoo.com>
2020-07-06 18:50:51 -0700

    Committed 005_Functions template

--- templates/ITVDN_CPP/CPP_Starter/007_Inctroduction to function/001_Functions/005_Functions/005_Functions.cpp
+++ templates/ITVDN_CPP/CPP_Starter/007_Inctroduction to function/001_Functions/005_Functions/005_Functions.cpp
@@ -1,24 +1,23 @@
-? *s..
-? |i..
-? |s..
-
-u.. s..
-
-s.. Function();
-
-in. main()
-{
-	s.. value _ Function();
-
-	c__ __  "Hello, " __  value __  e..
-
-	sy.. pause
-	r_ 0;
-}
-
-s.. Function()
-{
-	s.. value _ "Admin!";
-
-	r_ value;
-}
+//? *s..
+//? |i..
+//? |s..
+//
+//u.. s..
+//
+//s.. Function
+//
+//in. main
+//
+//	s.. value _ F..
+//
+//	c__ __  "Hello, " __  ? __  e..
+//
+//	sy.. pause
+//	r_ _
+//
+//
+//s.. Function
+//
+//	s.. value _ "Admin!"
+//
+//	r_ ?
