359bfefe9779dacee496bf9baf1743634e4a001e
Grey <sergejyurskyj@yahoo.com>
2020-07-06 18:56:49 -0700

    Committed 010_Functions template

--- templates/ITVDN_CPP/CPP_Starter/007_Inctroduction to function/001_Functions/010_Functions/010_Functions.cpp
+++ templates/ITVDN_CPP/CPP_Starter/007_Inctroduction to function/001_Functions/010_Functions/010_Functions.cpp
@@ -1,28 +1,28 @@
-? *s..
-? |i..
-? |s..
-
-u.. s..
-
-in. Mul(in. x, in. y);
-
-in. main()
-{
-	in. val1 _ 0, val2 _ 0, result;
-
-	c__ __  "Enter the number and the degree of the number in space or Enter: ";
-	c__ __ val1 __ val2;
-
-	result _ Mul(val1, val2);
-
-	c__ __  val1 __  " * " __  val2 __  " = " __  result __  e..
-
-	sy.. pause
-	r_ 0;
-}
-
-in. Mul(in. x, in. y)
-{
-	in. temp _ x * y;
-	r_ temp;
-}
+//? *s..
+//? |i..
+//? |s..
+//
+//u.. s..
+//
+//in. Mul in. x, in. y
+//
+//in. main
+//
+//	in. val1 _ 0, val2 _ 0, result
+//
+//	c__ __  "Enter the number and the degree of the number in space or Enter: "
+//	c__ __ ? __ ?
+//
+//	result _ ? ? ?
+//
+//	c__ __  ? __  " * " __  ? __  " = " __  ? __  e..
+//
+//	sy.. pause
+//	r_ _
+//
+//
+//in. Mul in. x, in. y
+//
+//	in. temp _ x * y
+//	r_ temp
+//
