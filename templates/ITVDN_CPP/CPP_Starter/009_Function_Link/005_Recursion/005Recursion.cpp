1879474325f870b4034841660a6bfd5acf53df37
Grey <sergejyurskyj@yahoo.com>
2020-07-06 20:12:58 -0700

    Committed 005Recursion template

--- templates/ITVDN_CPP/CPP_Starter/009_Function_Link/005_Recursion/005Recursion.cpp
+++ templates/ITVDN_CPP/CPP_Starter/009_Function_Link/005_Recursion/005Recursion.cpp
@@ -1,37 +1,36 @@
-﻿? *s..
-? |i..
-
-u.. s..
-
-v.. recursion(in. counter)sy.. pause
-v.. func(in. counter)sy.. pause
-
-in. main()
-{
-	func(3)sy.. pause
-
-	sy.. pause
-	r_ 0sy.. pause
-}
-
-v.. recursion(in. counter)
-{
-	counter--sy.. pause
-
-	c__ __  "Forst part of recursion function " __  counter __  e..
-
-	__ (counter !_ 0)
-	{
-		func(counter)sy.. pause
-	}
-	c__ __  "Second part of recursion function " __  counter __  e..
-}
-
-v.. func(in. counter)
-{
-	c__ __  "First part of func function " __  counter __  e..
-
-	recursion(counter)sy.. pause
-
-	c__ __  "Second part of func function " __  counter __  e..
-}
+﻿//? *s..
+//? |i..
+//
+//u.. s..
+//
+//v.. ? in. counter sy.. pause
+//v.. ? in. counter sy.. pause
+//
+//in. main
+//
+//	? 3 sy.. pause
+//
+//	sy.. pause
+//	r_ _ sy.. pause
+//
+//
+//v.. recursion in. counter
+//
+//	?## sy.. pause
+//
+//	c__ __  "Forst part of recursion function " __  ? __  e..
+//
+//	__ ? !_ 0
+//
+//		? ? sy.. pause
+//
+//	c__ __  "Second part of recursion function " __  ? __  e..
+//
+//
+//v.. func in. counter
+//
+//	c__ __  "First part of func function " __  ? __  e..
+//
+//	? ? sy.. pause
+//
+//	c__ __  "Second part of func function " __  ? __  e..
