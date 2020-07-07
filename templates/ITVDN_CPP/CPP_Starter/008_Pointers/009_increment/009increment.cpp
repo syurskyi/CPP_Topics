edb12a064d1d1fde7e24bb29e092c257dc988548
Grey <sergejyurskyj@yahoo.com>
2020-07-06 19:50:15 -0700

    Committed 009increment template

--- templates/ITVDN_CPP/CPP_Starter/008_Pointers/009_increment/009increment.cpp
+++ templates/ITVDN_CPP/CPP_Starter/008_Pointers/009_increment/009increment.cpp
@@ -1,34 +1,34 @@
-? *s..
-? |i..
-
-u.. s..
-
-in. main()
-{
-	c__ __  "Enter the integer number: "
-	in. count _ 0
-	c__ __ count
-
-	in. *block _ new in.[count]
-	in. *pBlock _ block
-
-	c__ __  "Fill the block: " __  e..
-
-	___ (in. i _ 0 i < count i++)
-	{
-		c__ __  "Enter number: "
-		c__ __ *(block + i)
-	}
-
-	c__ __  "Display all numbers in block: " __  e..
-	___ (in. i _ 0 i < count i++)
-	{
-		c__ __  *(pBlock++) __  " "
-	}
-	c__ __  e..
-
-	delete[] block
-
-	sy.. pause
-	r_ 0
-}
+//? *s..
+//? |i..
+//
+//u.. s..
+//
+//in. main
+//
+//	c__ __  "Enter the integer number: "
+//	in. count _ 0
+//	c__ __ ?
+//
+//	in. &block _ ne. in.|?
+//	in. &B.. _ b..
+//
+//	c__ __  "Fill the block: " __  e..
+//
+//	___ (in. i _ 0 ? < ? ###
+//
+//		c__ __  "Enter number: "
+//		c__ __ & b.. + ?
+//
+//
+//	c__ __  "Display all numbers in block: " __  e..
+//	___ (in. i _ 0 ? < ? ###
+//
+//		c__ __  & B..## __  " "
+//
+//	c__ __  e..
+//
+//	d..|| b..
+//
+//	sy.. pause
+//	r_ _
+//
