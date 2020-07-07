b1fa21bd302c45a13d31cb75828eb02283f61b70
Grey <sergejyurskyj@yahoo.com>
2020-07-06 19:42:55 -0700

    Committed 005Pointer template

--- templates/ITVDN_CPP/CPP_Starter/008_Pointers/005_Pointer/005Pointer.cpp
+++ templates/ITVDN_CPP/CPP_Starter/008_Pointers/005_Pointer/005Pointer.cpp
@@ -1,23 +1,23 @@
-? *s..
-? |i..
-
-u.. s..
-
-in. main()
-{
-	in. age _ 25
-	in. *pAge _ &age
-
-	c__ __  "Age equals " __  age __  e..
-	c__ __  "Address of variable age: 0x" __  pAge __  e..
-
-	c__ __  "Enter an age: "
-	c__ __ *pAge
-
-	c__ __  "New age equals " __  age __  e..
-	c__ __  "New value has 0x" __  pAge __  " address" __  e..
-
-	sy.. pause
-	r_ 0
-}
-
+//? *s..
+//? |i..
+//
+//u.. s..
+//
+//in. main
+//
+//	in. age _ 25
+//	in. &? _ *?
+//
+//	c__ __  "Age equals " __  ? __  e..
+//	c__ __  "Address of variable age: 0x" __  ? __  e..
+//
+//	c__ __  "Enter an age: "
+//	c__ __ &?
+//
+//	c__ __  "New age equals " __  ? __  e..
+//	c__ __  "New value has 0x" __  ? __  " address" __  e..
+//
+//	sy.. pause
+//	r_ _
+//
+//
