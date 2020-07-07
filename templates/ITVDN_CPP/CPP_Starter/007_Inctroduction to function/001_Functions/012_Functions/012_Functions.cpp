3ffa990bc65e1968c358a091c659a6755356c772
Grey <sergejyurskyj@yahoo.com>
2020-07-06 19:00:02 -0700

    Committed 012_Functions template

--- templates/ITVDN_CPP/CPP_Starter/007_Inctroduction to function/001_Functions/012_Functions/012_Functions.cpp
+++ templates/ITVDN_CPP/CPP_Starter/007_Inctroduction to function/001_Functions/012_Functions/012_Functions.cpp
@@ -1,36 +1,35 @@
-? *s..
-? |i..
-? |s..
-
-u.. s..
-
-v.. SampleFunction();
-v.. Global();
-
-s.. value _ "Global variable!";
-
-in. main()
-{
-	s.. value _ "Local variable in Main!";
-
-	c__ __  value __  e..
-
-	SampleFunction();
-
-	Global();
-
-	c__.g..
-    r_ 0;
-}
-
-v.. SampleFunction()
-{
-	s.. value _ "Local variable in SampleFunction!";
-
-	c__ __  value __  e..
-}
-
-v.. Global()
-{
-	c__ __  value __  e..
-}
+//? *s..
+//? |i..
+//? |s..
+//
+//u.. s..
+//
+//v.. S..
+//v.. G..
+//
+//s.. value _ "Global variable!"
+//
+//in. main
+//
+//	s.. value _ "Local variable in Main!"
+//
+//	c__ __  ? __  e..
+//
+//	?
+//
+//	?
+//
+//	c__.g..
+//    r_ _
+//
+//
+//v.. SampleFunction
+//
+//	s.. value _ "Local variable in SampleFunction!"
+//
+//	c__ __  ? __  e..
+//
+//
+//v.. Global
+//
+//	c__ __  value __  e..
