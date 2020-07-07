59e180a7f28764c51547eec927243b87ac78651b
Grey <sergejyurskyj@yahoo.com>
2020-07-06 18:10:52 -0700

    Committed 011_Switch template

--- templates/ITVDN_CPP/CPP_Starter/004_Conditionals/011_Switch/011_Switch.cpp
+++ templates/ITVDN_CPP/CPP_Starter/004_Conditionals/011_Switch/011_Switch.cpp
@@ -1,40 +1,40 @@
-? *s..
-? |i..
-
-u.. s..
-
-in. main()
-{
-    setlocale(LC_ALL, "rus");
-
-    c__ __  "Enter the day number of the week: " __  e..
-
-    in. dayNumber;
-    c__ __ dayNumber;
-
-    s.. (dayNumber)
-    {
-        c__:
-        c..
-        c.. 3:
-        c.. 4:
-        c.. 5:
-        {
-            c__ __  "Working day." __  e..
-            b..
-        }
-        c.. 6:
-        c.. 7:
-        {
-            c__ __  "Day off" __  e..
-            b..
-        }
-        default:
-        {
-            c__ __  "There is no such day of the week." __  e..
-            b..
-        }            
-    }
-    r_ 0;
-}
-
+//? *s..
+//? |i..
+//
+//u.. s..
+//
+//in. main
+//
+//    setlocale(LC_ALL, "rus");
+//
+//    c__ __  "Enter the day number of the week: " __  e..
+//
+//    in. dayNumber
+//    c__ __ ?
+//
+//    s.. ?
+//
+//        c__
+//        c..
+//        c.. 3
+//        c.. 4
+//        c.. 5
+//
+//            c__ __  "Working day." __  e..
+//            b..
+//
+//        c.. 6:
+//        c.. 7:
+//
+//            c__ __  "Day off" __  e..
+//            b..
+//
+//        d..
+//
+//            c__ __  "There is no such day of the week." __  e..
+//            b..
+//        }
+//
+//    r_ _
+//
+//
