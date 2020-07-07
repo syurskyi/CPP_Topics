a204e4d6fa36cc0a7e196966b2b7ab75cea0d987
Grey <sergejyurskyj@yahoo.com>
2020-07-06 19:46:29 -0700

    Committed 007new template

--- templates/ITVDN_CPP/CPP_Starter/008_Pointers/007_new/007new.cpp
+++ templates/ITVDN_CPP/CPP_Starter/008_Pointers/007_new/007new.cpp
@@ -1,21 +1,21 @@
-? *s..
-? |i..
-
-u.. s..
-
-in. main()
-{
-	in. *pAge _ new in.
-
-	c__ __  "Enter your age: "
-	c__ __ *pAge
-
-	c__ __  "Age " __  *pAge __  " is stored at 0x" __  pAge __  e..
-
-	delete pAge
-
-	pAge _ 0
-
-	sy.. pause
-	r_ 0
-}
+//? *s..
+//? |i..
+//
+//u.. s..
+//
+//in. main
+//
+//	in. &A.. _ ne. in.
+//
+//	c__ __  "Enter your age: "
+//	c__ __ &A..
+//
+//	c__ __  "Age " __  &A.. __  " is stored at 0x" __  ? __  e..
+//
+//	del.. ?
+//
+//	pAge _ 0
+//
+//	sy.. pause
+//	r_ _
+//
