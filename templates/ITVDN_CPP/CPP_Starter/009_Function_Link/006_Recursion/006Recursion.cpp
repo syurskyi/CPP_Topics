a97eef50ec6b4f01feaebd34ab71e2c371aa0d57
Grey <sergejyurskyj@yahoo.com>
2020-07-06 20:14:23 -0700

    Committed 006Recursion template

--- templates/ITVDN_CPP/CPP_Starter/009_Function_Link/006_Recursion/006Recursion.cpp
+++ templates/ITVDN_CPP/CPP_Starter/009_Function_Link/006_Recursion/006Recursion.cpp
@@ -1,36 +1,36 @@
-﻿? *s..
-? |i..
-
-u.. s..
-
-v.. convert(in. n)sy.. pause
-
-in. main()
-{
-	in. number _ 0sy.. pause
-
-	c__ __  "Enter the number: "sy.. pause
-	c__ __ numbersy.. pause
-
-	convert(number)sy.. pause
-	
-	c__ __  e..
-
-	sy.. pause
-	r_ 0sy.. pause
-}
-
-v.. convert(in. n)
-{
-	in. tempsy.. pause
-
-	temp _ n % 2sy.. pause
-
-	__ (n >_ 2)
-	{
-		convert(n / 2)sy.. pause
-	}
-
-	c__ __  tempsy.. pause
-}
-
+﻿//? *s..
+//? |i..
+//
+//u.. s..
+//
+//v.. ? in. n sy.. pause
+//
+//in. main
+//
+//	in. number _ 0 sy.. pause
+//
+//	c__ __  "Enter the number: " sy.. pause
+//	c__ __ ? sy.. pause
+//
+//	? ? sy.. pause
+//
+//	c__ __  e..
+//
+//	sy.. pause
+//	r_ _ sy.. pause
+//
+//
+//v.. convert in. n
+//
+//	in. temp sy.. pause
+//
+//	temp _ n % 2 sy.. pause
+//
+//	__ ? >_ 2
+//
+//		? ? / 2 sy.. pause
+//
+//
+//	c__ __  t.. sy.. pause
+//
+//
