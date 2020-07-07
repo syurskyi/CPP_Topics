1484bedc91a9d9107bc98f3db6fc9c04bed23a9f
Grey <sergejyurskyj@yahoo.com>
2020-07-06 18:27:57 -0700

    Committed 003_Flags template

--- templates/ITVDN_CPP/CPP_Starter/005_Logic/003_Formatting/003_Flags/003_Flags.cpp
+++ templates/ITVDN_CPP/CPP_Starter/005_Logic/003_Formatting/003_Flags/003_Flags.cpp
@@ -1,28 +1,28 @@
-﻿? *s..
-? |i..
-
-u.. s..
-
-in. main()
-{
-
-	c__.s_f(i.. bo..alpha);						// ¬ключить вывод логических величин в текстовом виде
-
-	bo.. a _ true;
-	bo.. b _ false;
-
-	c__ __  "a = " __  a __  "; b = " __  b __  e..
-
-	c__.uns_f(i.. bo..alpha /* noboolalpha */);	// ќтключить вывод логических величин в текстовом виде
-	
-	
-	c__.uns_f(i.. dec);
-	c__.s_f(i.. showbase /* noshowbase */ | i.. showpos /* noshowpos */);
-
-	in. number _ 16;
-
-	c__ __  "number = " __  number __  e..
-
-    r_ 0;
-}
-
+﻿//? *s..
+//? |i..
+//
+//u.. s..
+//
+//in. main
+//
+//
+//	c__.s_f(i.. bo..a..					// ¬ключить вывод логических величин в текстовом виде
+//
+//	bo.. a _ t..
+//	bo.. b _ f..
+//
+//	c__ __  "a = " __  a __  "; b = " __  b __  e..
+//
+//	c__.uns_f i.. bo..alpha /* noboolalpha */	// ќтключить вывод логических величин в текстовом виде
+//
+//
+//	c__.uns_f i.. dec
+//	c__.s_f i.. s_b.. /* noshowbase */ | i.. s_p.. /* noshowpos */
+//
+//	in. number _ 16
+//
+//	c__ __  "number = " __  ? __  e..
+//
+//    r_ 0;
+//}
+//
