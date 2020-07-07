36cba11a1b9826c917447fccde5e7464830d2a02
Grey <sergejyurskyj@yahoo.com>
2020-07-06 18:04:33 -0700

    Committed 007_Ternary template

--- templates/ITVDN_CPP/CPP_Starter/004_Conditionals/007_Ternary/007_Ternary.cpp
+++ templates/ITVDN_CPP/CPP_Starter/004_Conditionals/007_Ternary/007_Ternary.cpp
@@ -4,15 +4,15 @@
 
 u.. s..
 
-in. main()
-{
-    in. a _ 8, b _ 7, c _ 1, max _ -1;
+in. main
 
-    max _ (a < b) ? c _ b : c _ a;  //сачала с = b, а только потом max = с
+    in. a _ 8, b _ 7, c _ 1, max _ -1
+
+    max _ (a < b) | c _ b | c _ a //сачала с = b, а только потом max = с
 
     c__ __  max __  e..
 
     c__.g..
-    r_ 0;
-}
+    r_ _
+
 
