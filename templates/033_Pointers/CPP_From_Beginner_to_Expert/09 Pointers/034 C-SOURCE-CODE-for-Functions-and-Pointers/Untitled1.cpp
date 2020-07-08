3ef50020f822482724e163e4ef85bd82b03ee0e0
Grey <sergejyurskyj@yahoo.com>
2020-07-08 13:21:03 -0700

    Committed 034 C-SOURCE-CODE-for-Functions-and-Pointers templates

--- templates/033_Pointers/CPP_From_Beginner_to_Expert/09 Pointers/034 C-SOURCE-CODE-for-Functions-and-Pointers/Untitled1.cpp
+++ templates/033_Pointers/CPP_From_Beginner_to_Expert/09 Pointers/034 C-SOURCE-CODE-for-Functions-and-Pointers/Untitled1.cpp
@@ -1,48 +1,48 @@
-? |i..
-
-u.. s..
-
-//void multiplyBy(int &, int);
-in. * multiplyBy(in. *, in.)
-v.. multiplyArrayBy(in. *, in., in.)
-
-in. main()
-{
-    /*
-    int a = 10;
-
-    int *b = multiplyBy(&a, 5);
-
-    *b = 999;
-    cout << a << endl;
-    cout << *b << endl;
-    */
-
-    in. array[10]
-    //cout << sizeof(array)/sizeof(array[0]) << endl;
-
-    ___ (in. i _ 0 i < s_o_(array)/s_o_(array[0]) ###)
-    {
-        array[i] _ i
-     //   cout << "array [" << i << "] = " << array[i] << endl;
-    }
-    multiplyArrayBy(&array[0], 5, s_o_(array)/s_o_(array[0])) // &array[0] == array
-    ___ (in. i _ 0 i < s_o_(array)/s_o_(array[0]) ###)
-    {
-       c.. __ "array [" __ i __ "] = " __ array[i] __ e..
-    }
-
-    r_ _
-}
-in. * multiplyBy(in. * var, in. amount)
-{
-    //int * var = &a;
-    *var _  *var * amount
-
-    r_ var
-}
-v.. multiplyArrayBy(in. *array, in. amount, in. sizeOfArray)
-{
-    w___(sizeOfArray--)
-        array[sizeOfArray] *_ amount
-}
+//? |i..
+//
+//u.. s..
+//
+////void multiplyBy(int &, int);
+//in. # multiplyBy in. #, in.
+//v.. multiplyArrayBy in. #, in., in.
+//
+//in. main
+//
+//    /*
+//    int a = 10;
+//
+//    int *b = multiplyBy(&a, 5);
+//
+//    *b = 999;
+//    cout << a << endl;
+//    cout << *b << endl;
+//    */
+//
+//    in. array 10
+//    //cout << sizeof(array)/sizeof(array[0]) << endl;
+//
+//    ___ in. i _ 0 ? < s_o_|?| /s_o_|? 0 ###
+//
+//        ? ? _ ?
+//     //   cout << "array [" << i << "] = " << array[i] << endl;
+//
+//    mAB_ #? 0 , 5, s_o_|?|/s_o_|? 0 // &array[0] == array
+//    ___ in. i _ 0 ? < s_o_|?|/s_o_|? 0 ###
+//
+//       c.. __ "array [" __ ? __ "] = " __ ? i __ e..
+//
+//
+//    r_ _
+//
+//in. # multiplyBy in. # var, in. amount
+//
+//    //int * var = &a;
+//    #v.. _  #v.. * a..
+//
+//    r_ v..
+//
+//v.. multiplyArrayBy in. #array, in. amount, in. sizeOfArray
+//
+//    w___ s..##
+//        a.. s.. *_ a..
+//
