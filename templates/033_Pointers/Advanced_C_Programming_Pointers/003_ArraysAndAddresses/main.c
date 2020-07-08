52da18fb9baff9a8f4028f93adbcdd32a06a4f94
Grey <sergejyurskyj@yahoo.com>
2020-07-08 15:57:11 -0700

    Committed 003_ArraysAndAddresses templates

--- templates/033_Pointers/Advanced_C_Programming_Pointers/003_ArraysAndAddresses/main.c
+++ templates/033_Pointers/Advanced_C_Programming_Pointers/003_ArraysAndAddresses/main.c
@@ -1,11 +1,11 @@
-? |s..
-
-in. main(in. argc, ch.. @@ {
-	ch.. str1[] = "Hello";
-	ch.. *str2 = "Goodbye";
-	// str2 = &str1;
-	// str2 = str1;
-	p..("%d %d %s\n", &str1, str1, str1);
-	p..("%d %d %s\n", &str2, str2, str2);
-	r_ 0;
-}
+//? |s..
+//
+//in. main in. argc, ch.. @@
+//	ch.. str1|| = "Hello"
+//	ch.. *str2 = "Goodbye"
+//	// str2 = &str1;
+//	// str2 = str1;
+//	p.. "@d @d @s##" #? ? ?
+//	p.. "@d @d @s##" #? ? ?
+//	r_ _
+//
