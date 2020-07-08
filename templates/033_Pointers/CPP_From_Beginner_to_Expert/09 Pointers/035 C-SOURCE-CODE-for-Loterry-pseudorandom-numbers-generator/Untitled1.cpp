d9eeb622e220f2a56206b12f48c1c17c53de23b3
Grey <sergejyurskyj@yahoo.com>
2020-07-08 15:41:56 -0700

    Committed 035 C-SOURCE-CODE-for-Loterry-pseudorandom-numbers-generator templates

--- templates/033_Pointers/CPP_From_Beginner_to_Expert/09 Pointers/035 C-SOURCE-CODE-for-Loterry-pseudorandom-numbers-generator/Untitled1.cpp
+++ templates/033_Pointers/CPP_From_Beginner_to_Expert/09 Pointers/035 C-SOURCE-CODE-for-Loterry-pseudorandom-numbers-generator/Untitled1.cpp
@@ -1,45 +1,45 @@
-? |i..
-
-u.. s..
-/* LOTTERY - pseudo-random numbers generator - drawing numbers */
-v.. lottery(in., in.)
-
-in. main()
-{
-//  srand(time(NULL)); //seeds
-
- //   int nr = rand() % 49 + 1; //[1, 49]
-
-   // cout << nr << endl;
-
-    lottery(49, 6)
-
-    r_ _
-}
-v.. lottery(in. total_balls, in. balls_to_draw)
-{
-    __ (total_balls < balls_to_draw)
-        r_
-
-     srand(time(NULL)) //seeds
-
-     in. *balls _ n.. in.[balls_to_draw]
-
-     ___ (in. i _ 0 i < balls_to_draw ###)
-     {
-         balls[i] _ rand() % total_balls + 1
-
-         ___ (in. j _ 0 j < i + 1 j++)
-         {
-            __(balls[i] __ balls[j] && i !_ j)
-            {
-                i--
-                b..
-            }
-            ____ __ (j __ i)
-                c.. __ balls[i] __ e..
-         }
-     }
-
-     de..[] balls
-}
+//? |i..
+//
+//u.. s..
+///* LOTTERY - pseudo-random numbers generator - drawing numbers */
+//v.. lottery in., in.
+//
+//in. main
+//
+////  srand(time(NULL)); //seeds
+//
+// //   int nr = rand() % 49 + 1; //[1, 49]
+//
+//   // cout << nr << endl;
+//
+//    ? 49, 6
+//
+//    r_ _
+//
+//v.. lottery in. total_balls, in. balls_to_draw
+//
+//    __ t.. < b..
+//        r_
+//
+//     sra.. ti.. N.. //seeds
+//
+//     in. #balls _ n.. in.|b..
+//
+//     ___ in. i _ 0 ? < b.. ###
+//
+//         b.. ? _ ra..  % t.. + 1
+//
+//         ___ in. j _ 0 ? < i + 1 ###
+//
+//            __ b.. ? __ b.. ? ## ? !_ ?
+//
+//                @##
+//                b..
+//
+//            ____ __ j __ i
+//                c.. __ b.. ? __ e..
+//
+//
+//
+//     de..[] b..
+//
