7c650a52a2e0e5b3e04c23e6b2c1eba57db97e1f
Grey <sergejyurskyj@yahoo.com>
2020-07-08 12:50:33 -0700

    Committed 013_Arrays-And-Functions templates

--- templates/033_Pointers/05 - Pointers and Memory/013_Arrays-And-Functions/Arrays and Functions/src/Arrays_and_Functions.cpp
+++ templates/033_Pointers/05 - Pointers and Memory/013_Arrays-And-Functions/Arrays and Functions/src/Arrays_and_Functions.cpp
@@ -1,61 +1,60 @@
-//============================================================================
-// Name        : Arrays.cpp
-// Author      : Serhii Yurskyi
-// Version     :
-//============================================================================
-
-? |i..
-u.. s..
-
-// Could declare variables here! string numbers[] = {"one", "two", "three"};
-
-v.. show1(c.. in. nElements, st.. texts[]) {
-
-	// cout << sizeof(texts) << endl; returns sizeof pointer!
-
-	___(in. i_0 i<nElements i++) {
-		c.. __ texts[i] __ e..
-	}
-}
-
-v.. show2(c.. in. nElements, st.. *texts) {
-
-	// cout << sizeof(texts) << endl; returns sizeof pointer!
-
-	___(in. i_0 i<nElements i++) {
-		c.. __ texts[i] __ e..
-	}
-}
-
-v.. show3(st.. (&texts)[3]) {
-
-	// cout << sizeof(texts) << endl; returns sizeof pointer!
-
-	___(in. i_0 i<s_o_(texts)/s_o_(st..) i++) {
-		c.. __ texts[i] __ e..
-	}
-}
-
-ch.. *getMemory() {
-	ch.. *pMem _ n.. ch..[100]
-
-	r_ pMem
-}
-
-v.. freeMemory(ch.. *pMem) {
-	delete [] pMem
-}
-
-in. main() {
-
-	st.. texts[] _ {"apple", "orange", "banana"}
-
-	c.. __ s_o_(texts) __ e..
-
-	show3(texts)
-
-	ch.. *pMemory _ getMemory()
-	freeMemory(pMemory)
-
-	r_ _
-}
+////============================================================================
+//// Name        : Arrays.cpp
+//// Author      : Serhii Yurskyi
+//// Version     :
+////============================================================================
+//
+//? |i..
+//u.. s..
+//
+//// Could declare variables here! string numbers[] = {"one", "two", "three"};
+//
+//v.. show1 c.. in. nElements, st.. texts
+//
+//	// cout << sizeof(texts) << endl; returns sizeof pointer!
+//
+//	___ in. i_0 ?<n.. ###
+//		c.. __ t.. ? __ e..
+//
+//
+//
+//v.. show2 c.. in. nElements, st.. #texts
+//
+//	// cout << sizeof(texts) << endl; returns sizeof pointer!
+//
+//	___ in. i_0 ?<n.. ###
+//		c.. __ t.. ? __ e..
+//
+//
+//v.. show3 st.. #texts 3
+//
+//	// cout << sizeof(texts) << endl; returns sizeof pointer!
+//
+//	___ in. i_0 ?<s_o_(t..)/s_o_(st..) ###
+//		c.. __ t.. ? __ e..
+//
+//
+//
+//ch.. #getMemory
+//	ch.. #pMem _ n.. ch.. 100
+//
+//	r_ _?
+//
+//
+//v.. freeMemory ch.. #pMem
+//	de.. || _?
+//}
+//
+//in. main
+//
+//	st.. texts|| _ "apple", "orange", "banana"
+//
+//	c.. __ s_o_(?) __ e..
+//
+//	_3 ?
+//
+//	ch.. #_M.. _ gM..
+//	fM.. _M..
+//
+//	r_ _
+//
