73aa8148b3e1e761d6338013aae8085847d2aca4
Grey <sergejyurskyj@yahoo.com>
2020-07-08 12:16:05 -0700

    Committed 007_References templates

--- templates/033_Pointers/05 - Pointers and Memory/007_References/src/References.cpp
+++ templates/033_Pointers/05 - Pointers and Memory/007_References/src/References.cpp
@@ -1,28 +1,27 @@
-//============================================================================
-// Name        : 007_References.cpp
-// Author      : Serhii Yurskyi
-// Version     :
-//============================================================================
-
-? |i..
-u.. s..
-
-v.. changeSomething(d.. &value) {
-	value _ 123.4
-}
-
-in. main() {
-
-	in. value1 _ 8
-	in. &value2 _ value1
-	value2 _ 10
-
-	c.. __ "Value1: " __ value1 __ e..
-	c.. __ "Value2: " __ value2 __ e..
-
-	d.. value _ 4.321
-	changeSomething(value)
-	c.. __ value __ e..
-
-	r_ _
-}
+////============================================================================
+//// Name        : 007_References.cpp
+//// Author      : Serhii Yurskyi
+//// Version     :
+////============================================================================
+//
+//? |i..
+//u.. s..
+//
+//v.. changeSomething(d.. #value
+//	value _ 123.4
+//
+//
+//in. main
+//
+//	in. value1 _ 8
+//	in. #value2 _ ?
+//	value2 _ 10
+//
+//	c.. __ "Value1: " __ ? __ e..
+//	c.. __ "Value2: " __ ? __ e..
+//
+//	d.. value _ 4.321
+//	? ?
+//	c.. __ ? __ e..
+//
+//	r_ _
