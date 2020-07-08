a29fa01523eb470dfae1549cc4df0028f18ae640
Grey <sergejyurskyj@yahoo.com>
2020-07-08 13:14:17 -0700

    Committed 033 C-SOURCE-CODE-for-Strings-and-pointer-on-a-char-type templates

--- templates/033_Pointers/CPP_From_Beginner_to_Expert/09 Pointers/033 C-SOURCE-CODE-for-Strings-and-pointer-on-a-char-type/Untitled1.cpp
+++ templates/033_Pointers/CPP_From_Beginner_to_Expert/09 Pointers/033 C-SOURCE-CODE-for-Strings-and-pointer-on-a-char-type/Untitled1.cpp
@@ -1,70 +1,70 @@
-? |i..
-
-u.. s..
-
-in. main()
-{
-    st.. text _ "this a test" // t r a l a \0
-
-/*
-    for (int i = 0; i < text.length(); i++)
-    {
-        cout << text[i] << endl;
-    }
-*/
-  /*  char ch = text[0];
-
-   // cout << ch << endl;
-
-    char characters[] = "123asdfasdfasdf4";
-
-    cout << characters[0] << endl;
-    cout << *(characters) << endl;
-
-    cout << characters[1] << endl;
-    cout << *(characters+1) << endl;
-
-    char *p = characters;
-
-    cout << p[0] << endl;
-    cout << *(p) << endl;
-
-    cout << p[1] << endl;
-    cout << *(p+1) << endl;
-*/
-    c.. ch.. * text2 _ text.c_str()
-
-    c.. __ text2 __ e..
-
-    ch.. array[] _ "here is a text"
-
-    st.. test _ array
-
-    c.. __ test __ e..
-
-
-    c.. ch.. * a _ "this is a test 12512412"
-
-    c.. __ a[0] __ e..
-
-    ch.. b[] _ "this a test 124124"
-
-    b[0] _'g'
-    c.. __ b __ e..
-
-    ch.. * c.. dynamic_array _ n.. ch..[50]
-    dynamic_array[0] _ 'k'
-    dynamic_array[1] _ '\0'
-    //dynamic_array = "lalala";
-
-    c.. __ dynamic_array __ e..
-
-    de.. [] dynamic_array
-
-    st.. array_of_string[5] _ "this i a text that will be in all of the elements of strings"
-    array_of_string[0] _ "afsdf"
-    c.. __ array_of_string[0] __ e..
-    c.. __ array_of_string[1] __ e..
-    c.. __ array_of_string[2] __ e..
-    r_ _
-}
+//? |i..
+//
+//u.. s..
+//
+//in. main
+//
+//    st.. text _ "this a test" // t r a l a \0
+//
+///*
+//    for (int i = 0; i < text.length(); i++)
+//    {
+//        cout << text[i] << endl;
+//    }
+//*/
+//  /*  char ch = text[0];
+//
+//   // cout << ch << endl;
+//
+//    char characters[] = "123asdfasdfasdf4";
+//
+//    cout << characters[0] << endl;
+//    cout << *(characters) << endl;
+//
+//    cout << characters[1] << endl;
+//    cout << *(characters+1) << endl;
+//
+//    char *p = characters;
+//
+//    cout << p[0] << endl;
+//    cout << *(p) << endl;
+//
+//    cout << p[1] << endl;
+//    cout << *(p+1) << endl;
+//*/
+//    c.. ch.. # text2 _ ?.c_str
+//
+//    c.. __ _2 __ e..
+//
+//    ch.. array|| _ "here is a text"
+//
+//    st.. test _ ?
+//
+//    c.. __ ? __ e..
+//
+//
+//    c.. ch.. # a _ "this is a test 12512412"
+//
+//    c.. __ ? 0 __ e..
+//
+//    ch.. b|| _ "this a test 124124"
+//
+//    ? 0 _'g'
+//    c.. __ ? __ e..
+//
+//    ch.. # c.. dynamic_array _ n.. ch.. 50
+//    ? 0 _ 'k'
+//    ? 1 _ '\0'
+//    //dynamic_array = "lalala";
+//
+//    c.. __ ? __ e..
+//
+//    de.. || ?
+//
+//    st.. array_of_string 5 _ "this i a text that will be in all of the elements of strings"
+//    ? 0 _ "afsdf"
+//    c.. __ ? 0 __ e..
+//    c.. __ ? 1 __ e..
+//    c.. __ ? 2 __ e..
+//    r_ _
+//
