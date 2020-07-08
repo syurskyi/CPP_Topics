0333c12832d6d343215dd3418f7f4a438adb592b
Grey <sergejyurskyj@yahoo.com>
2020-07-08 16:20:09 -0700

    Committed 006_CommandlineArgs templates

--- templates/033_Pointers/Advanced_C_Programming_Pointers/006_CommandlineArgs/main.c
+++ templates/033_Pointers/Advanced_C_Programming_Pointers/006_CommandlineArgs/main.c
@@ -1,25 +1,25 @@
-? |s..
-
-/* This program displays any 
- * arguments that were passed to it 
- */
-
-in. main(in. argc, ch.. @@ {
-    in. i;
-        
-    // (1) iterate over array of args
-    ___ (in. i = 0; i < argc; ###) {
-        p..("arg %d is %s\n", i, argv[i]);	// prints arg at index i
-	}
-    
-    p..("\n\n");
-    
-    // 
-	// (2) dereference each string arg (*argv) via pointer to the pointer  
-    // to the start of the array of args (**argv)
-	___ (i = 0; i < argc; ###) {
-		p..("arg %d is %s\n", i, *argv);	// prints arg pointed to by argv
-        argv += 1;	
-	}
- 	r_ 0;
-}
+//? |s..
+//
+///* This program displays any
+// * arguments that were passed to it
+// */
+//
+//in. main in. argc, ch.. @@
+//    in. i
+//
+//    // (1) iterate over array of args
+//    ___ in. i _ 0 ? < a.. ###
+//        p..("arg %d is %s##", i, argv ?	// prints arg at index i
+//
+//
+//    p.. "####"
+//
+//    //
+//	// (2) dereference each string arg (*argv) via pointer to the pointer
+//    // to the start of the array of args (**argv)
+//	___ i _ 0 ? < a.. ###
+//		p.. "arg @d is @s##" ? @	// prints arg pointed to by argv
+//        a.. +_ 1;
+//
+// 	r_ _
+//
