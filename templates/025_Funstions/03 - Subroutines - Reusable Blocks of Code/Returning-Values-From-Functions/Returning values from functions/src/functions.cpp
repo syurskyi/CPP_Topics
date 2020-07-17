08f9a6633d464fc39722909c54f5fd601c6ebbd5
Grey <sergejyurskyj@yahoo.com>
2020-07-16 18:58:36 -0700

    Committed Returning-Values-From-Functions template

--- templates/025_Funstions/03 - Subroutines - Reusable Blocks of Code/Returning-Values-From-Functions/Returning values from functions/src/functions.cpp
+++ templates/025_Funstions/03 - Subroutines - Reusable Blocks of Code/Returning-Values-From-Functions/Returning values from functions/src/functions.cpp
@@ -1,45 +1,45 @@
-//============================================================================
-// Name        : functions.cpp
-// Author      : Serhii Yurskyi
-// Version     :
-//============================================================================
-
-? ios..
-u.. s..
-
-v.. showMenu
-	c.. __ "1. Search" __ e..
-	c.. __ "2. View Record" __ e..
-	c.. __ "3. Quit" __ e..
-}
-
-in. getInput
-	c.. __ "Enter selection: " __ e..
-
-	in. input;
-	ci. __ input;
-
-	r_ input;
-}
-
-in. main
-
-	showMenu();
-	in. selection = getInput();
-
-	sw.. (selection) {
-	ca.. 1:
-		c.. __ "Searching ..." __ e..
-		b..
-	ca.. 2:
-		c.. __ "Viewing ..." __ e..
-		b..
-	ca.. 3:
-		c.. __ "Quitting ..." __ e..
-		b..
-	def..
-		c.. __ "Please select an item from the menu." __ e..
-	}
-
-	r_ 0;
-}
+////============================================================================
+//// Name        : functions.cpp
+//// Author      : Serhii Yurskyi
+//// Version     :
+////============================================================================
+//
+//? ios..
+//u.. s..
+//
+//v.. showMenu
+//	c.. __ "1. Search" __ e..
+//	c.. __ "2. View Record" __ e..
+//	c.. __ "3. Quit" __ e..
+//
+//
+//in. getInput
+//	c.. __ "Enter selection: " __ e..
+//
+//	in. input
+//	ci. __ ?
+//
+//	r_ ?
+//
+//
+//in. main
+//
+//	s..
+//	in. selection _ g..
+//
+//	sw.. ?
+//	ca.. 1
+//		c.. __ "Searching ..." __ e..
+//		b..
+//	ca.. 2
+//		c.. __ "Viewing ..." __ e..
+//		b..
+//	ca.. 3
+//		c.. __ "Quitting ..." __ e..
+//		b..
+//	def..
+//		c.. __ "Please select an item from the menu." __ e..
+//
+//
+//	r_ _
+//
