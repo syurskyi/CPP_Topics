46076d0c41a8aa519941b709b22fd82dd377cae7
Grey <sergejyurskyj@yahoo.com>
2020-07-16 18:42:22 -0700

    Committed WhileLoop template

--- templates/013_Loop_and_ControlingProgramFlow/Section-9-Controling-Program-Flow/WhileLoop/main.cpp
+++ templates/013_Loop_and_ControlingProgramFlow/Section-9-Controling-Program-Flow/WhileLoop/main.cpp
@@ -1,58 +1,58 @@
-// Section 9
-// While Loop
-? ios..
-
-u.. s..
-
-in. main
-    
-    in. num {};
-    c.. __ "Enter a positive integer - start the countdown :";
-    ci. __ num;
-    
-    w__ (num > 0 ) {
-        c.. __ num __ e..
-       --num;
-    }
-    c.. __ "Blastoff!" __ e..
-    
-//    int num{};
-//    cout << "Enter a postive integer to count up to: ";
-//    cin >> num;
-//    
-//    int i {1};
-//    while (num >= i) {
-//        cout << i << endl;
-//        i++;
-//    }
-  
-//    int number {};
+//// Section 9
+//// While Loop
+//? ios..
 //
-//    cout << "Enter an integer less than 100: ";
-//    cin >> number;
+//u.. s..
 //
-//    while (number >= 100) {  // !(number < 100)
-//        cout << "Enter an integer less than 100: ";
-//        cin >> number;
-//    }
+//in. main
 //
-//    cout << "Thanks" << endl;  
-    
-//    bool done {false};
-//    int number {0};
+//    in. num
+//    c.. __ "Enter a positive integer - start the countdown :";
+//    ci. __ ?
 //
-//    while (!done) {
-//           cout << "Enter an integer between 1 and 5: ";
-//           cin >> number;
-//           if (number <=1 || number >=5)
-//               cout << "Out of range, try again" << endl;
-//           else {
-//               cout << "Thanks!" << endl;
-//               done = true;
-//            }
-//       }
-    
-    c..  __ e..
-    r_ _
-}
-
+//    w__ ? > 0
+//        c.. __ ? __ e..
+//       ##?
+//
+//    c.. __ "Blastoff!" __ e..
+//
+////    int num{};
+////    cout << "Enter a postive integer to count up to: ";
+////    cin >> num;
+////
+////    int i {1};
+////    while (num >= i) {
+////        cout << i << endl;
+////        i++;
+////    }
+//
+////    int number {};
+////
+////    cout << "Enter an integer less than 100: ";
+////    cin >> number;
+////
+////    while (number >= 100) {  // !(number < 100)
+////        cout << "Enter an integer less than 100: ";
+////        cin >> number;
+////    }
+////
+////    cout << "Thanks" << endl;
+//
+////    bool done {false};
+////    int number {0};
+////
+////    while (!done) {
+////           cout << "Enter an integer between 1 and 5: ";
+////           cin >> number;
+////           if (number <=1 || number >=5)
+////               cout << "Out of range, try again" << endl;
+////           else {
+////               cout << "Thanks!" << endl;
+////               done = true;
+////            }
+////       }
+//
+//    c..  __ e..
+//    r_ _
+//
+//
