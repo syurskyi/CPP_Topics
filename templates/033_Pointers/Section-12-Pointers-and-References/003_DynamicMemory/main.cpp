925c123d17594ef306b84612b45b80f966e5398a
Grey <sergejyurskyj@yahoo.com>
2020-07-17 11:56:05 -0700

    Committed 003_DynamicMemory template

--- templates/033_Pointers/Section-12-Pointers-and-References/003_DynamicMemory/main.cpp
+++ templates/033_Pointers/Section-12-Pointers-and-References/003_DynamicMemory/main.cpp
@@ -1,27 +1,27 @@
-// Section 12
-// Dynamic Memory
-? |i..
-
-u.. s..
-
-in. main
-    
-    in. *int_ptr {nullptr}
-    int_ptr _ n.. in.               // allocate the int on the heap
-    c.. __ int_ptr __ e..    // use it
-    de.. int_ptr                    // release it
-    
-    size_t size{0}
-    d.. *temp_ptr {nullptr}
-    
-    c.. __ "How many temps? "
-    ci. __ size
-    
-    temp_ptr _ n.. d..[size]    // allocate the storage on the heap
-    c.. __ temp_ptr __ e..       // use it
-    de.. [] temp_ptr                     // release it
-    
-    c.. __ e..
-    r_ _
-}
-
+//// Section 12
+//// Dynamic Memory
+//? |i..
+//
+//u.. s..
+//
+//in. main
+//
+//    in. #int_ptr nullptr
+//    ? _ n.. in.               // allocate the int on the heap
+//    c.. __ ? __ e..    // use it
+//    de.. ?                    // release it
+//
+//    size_t size 0
+//    d.. #temp_ptr nullptr
+//
+//    c.. __ "How many temps? "
+//    ci. __ s..
+//
+//    temp_ptr _ n.. d.. s..    // allocate the storage on the heap
+//    c.. __ ? __ e..       // use it
+//    de.. || ?                     // release it
+//
+//    c.. __ e..
+//    r_ _
+//
+//
