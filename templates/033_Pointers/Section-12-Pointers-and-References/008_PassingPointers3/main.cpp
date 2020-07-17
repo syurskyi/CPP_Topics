584ebe25a7cad0382d819ce46f7bc29188a6e5f8
Grey <sergejyurskyj@yahoo.com>
2020-07-17 12:10:00 -0700

    Committed 008_PassingPointers3 template

--- templates/033_Pointers/Section-12-Pointers-and-References/008_PassingPointers3/main.cpp
+++ templates/033_Pointers/Section-12-Pointers-and-References/008_PassingPointers3/main.cpp
@@ -1,38 +1,38 @@
-// Section 12
-// Passing Pointers 3
-
-? |i..
-? <st..>
-? ve..
-
-u.. s..
-
-v.. display( c.. vector<st..> *c..  v) {
-//(*v).at(0) = "Funny";
-    ___ (a.. str: *v)
-        c.. __ str __ " "
-    c.. __ e..
-  
- //   v = nullptr;
-}
-
-v.. display(in. *array, in. sentinel) {
-    w___ (*array !_ sentinel)
-        c.. __ *array++ __ " "
-    c.. __ e..
-}
-
-in. main
-
-//    cout << "-----------------------------" << endl;
-//    vector<string> stooges {"Larry", "Moe", "Curly"};
-//    display(&stooges);
-   
-    c.. __ "\n-----------------------------" __ e..
-    in. scores[] {100,98,97,79,85,-1}
-    display(scores, -1)
-    
-    c..  __ e..
-    r_ _
-}
-
+//// Section 12
+//// Passing Pointers 3
+//
+//? |i..
+//? st..
+//? ve..
+//
+//u.. s..
+//
+//v.. display( c.. ve..|st..| #c..  v
+////(*v).at(0) = "Funny";
+//    ___ (a.. str: #v
+//        c.. __ ? __ " "
+//    c.. __ e..
+//
+// //   v = nullptr;
+//
+//
+//v.. display(in. #array in. sentinel
+//    w___ (#? !_ ?
+//        c.. __ #?## __ " "
+//    c.. __ e..
+//
+//
+//in. main
+//
+////    cout << "-----------------------------" << endl;
+////    vector<string> stooges {"Larry", "Moe", "Curly"};
+////    display(&stooges);
+//
+//    c.. __ "\n-----------------------------" __ e..
+//    in. scores 100,98,97,79,85,-1
+//    ? ? -1)
+//
+//    c..  __ e..
+//    r_ _
+//
+//
