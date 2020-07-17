26f3e71dad119bff27ec3a93031f733c39e2afe4
Grey <sergejyurskyj@yahoo.com>
2020-07-17 11:58:24 -0700

    Committed 004_AraysAndPointers template

--- templates/033_Pointers/Section-12-Pointers-and-References/004_AraysAndPointers/main.cpp
+++ templates/033_Pointers/Section-12-Pointers-and-References/004_AraysAndPointers/main.cpp
@@ -1,37 +1,37 @@
-? |i..
-
-u.. s..
-
-in. main
-    
-    in. scores[] {100, 95, 89}
-    
-    c.. __ "Value of scores: " __ scores __ e..
-    
-    in. *score_ptr {scores}
-    c.. __ "Value of score_ptr: " __ score_ptr __ e..
-    
-    c.. __ "\nArray subscript notation -------------------------" __ e..
-    c.. __ scores[0] __ e..
-    c.. __ scores[1] __ e..
-    c.. __ scores[2] __ e..
-    
-    c.. __ "\nPointer subscript notation -------------------------" __ e..
-    c.. __ score_ptr[0] __ e..
-    c.. __ score_ptr[1] __ e..
-    c.. __ score_ptr[2] __ e..
-    
-    c.. __ "\nPointer offset notation-------------------------" __ e..
-    c.. __ *score_ptr __ e..
-    c.. __ *(score_ptr + 1) __ e..
-    c.. __ *(score_ptr +2) __ e..
-    
-    c.. __ "\nArray offset notation-------------------------" __ e..
-    c.. __ *scores __ e..
-    c.. __ *(scores + 1) __ e..
-    c.. __ *(scores +2) __ e..
-    
-    c.. __ e..
-    r_ _
-}
-
+//? |i..
+//
+//u.. s..
+//
+//in. main
+//
+//    in. scores|| 100, 95, 89
+//
+//    c.. __ "Value of scores: " __ ? __ e..
+//
+//    in. #score_ptr ?
+//    c.. __ "Value of score_ptr: " __ ? __ e..
+//
+//    c.. __ "\nArray subscript notation -------------------------" __ e..
+//    c.. __ ? 0 __ e..
+//    c.. __ ? 1 __ e..
+//    c.. __ ? 2 __ e..
+//
+//    c.. __ "\nPointer subscript notation -------------------------" __ e..
+//    c.. __ ? 0 __ e..
+//    c.. __ ? 1 __ e..
+//    c.. __ ? 2 __ e..
+//
+//    c.. __ "\nPointer offset notation-------------------------" __ e..
+//    c.. __ #? __ e..
+//    c.. __ #(? + 1) __ e..
+//    c.. __ #(? +2) __ e..
+//
+//    c.. __ "\nArray offset notation-------------------------" __ e..
+//    c.. __ #? __ e..
+//    c.. __ #(? + 1) __ e..
+//    c.. __ #(? +2) __ e..
+//
+//    c.. __ e..
+//    r_ _
+//
+//
