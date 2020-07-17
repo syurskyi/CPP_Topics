756c93c49a0df0b69528ec021b0f51faf909aa39
Grey <sergejyurskyj@yahoo.com>
2020-07-17 12:08:08 -0700

    Committed 007_PassingPointers2 template

--- templates/033_Pointers/Section-12-Pointers-and-References/007_PassingPointers2/main.cpp
+++ templates/033_Pointers/Section-12-Pointers-and-References/007_PassingPointers2/main.cpp
@@ -1,30 +1,30 @@
-// Section 12
-// Passing Pointers 2
-? |i..
-? <st..>
-? ve..
-
-u.. s..
-
-
-v.. swap(in. *a, in. *b) {
-    in. temp _ *a
-    *a _ *b
-    *b _ temp
-}
-
-
-in. main
-    in. x {100}, y {200}
-    c.. __ "\nx: " __ x __  e..
-    c.. __ "y: " __ y __  e..
-    
-    swap(&x, &y)
-    
-    c.. __ "\nx: " __ x __  e..
-    c.. __ "y: " __ y __  e..
-    
-    c..  __ e..
-    r_ _
-}
-
+//// Section 12
+//// Passing Pointers 2
+//? |i..
+//? <st..>
+//? ve..
+//
+//u.. s..
+//
+//
+//v.. swap in. #a, in. #b
+//    in. temp _ #a
+//    #a _ #b
+//    #b _ temp
+//
+//
+//
+//in. main
+//    in. x 100 y 200
+//    c.. __ "\nx: " __ x __  e..
+//    c.. __ "y: " __ y __  e..
+//
+//    swap #x, #y
+//
+//    c.. __ "\nx: " __ x __  e..
+//    c.. __ "y: " __ y __  e..
+//
+//    c..  __ e..
+//    r_ _
+//
+//
