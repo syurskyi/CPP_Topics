fd5b800fa1e22775f6ab467f3e051b444031eeb1
Grey <sergejyurskyj@yahoo.com>
2020-07-16 17:21:57 -0700

    Committed new examples

---
+++ examples/050_IO_and_Streams/Section-19-IO-and-Streams/Challenge_2/main.cpp
@@ -0,0 +1,14 @@
+// Section 19
+// 012_Challenge 2
+// Automated Grader
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    
+    cout << "Hello world" << endl;
+    return 0;
+}
+
