b5e3e6e6ef079da176101d7b0aa14d3bb5b2075d
Grey <sergejyurskyj@yahoo.com>
2020-07-16 18:16:56 -0700

    Committed Grades template

--- templates/013_Loop_and_ControlingProgramFlow/Section-9-Controling-Program-Flow/Grades/main.cpp
+++ templates/013_Loop_and_ControlingProgramFlow/Section-9-Controling-Program-Flow/Grades/main.cpp
@@ -1,47 +1,47 @@
-// Section 9
-// Grades
-
-/*
-    Calculate a sudent's grade on an exam given their score
-    and tell them if they passed the course
-    
-*/
-
-? ios..
-
-u.. s..
-
-in. main
-    
-    in. score {};
-    c.. __ "Enter your score on the exam (0-100) : ";
-    ci. __ score;
-    ch.. letter_grade {};
-    
-    __ (score >= 0 && score <=100) {
-        __ (score >= 90)
-            letter_grade = 'A';
-        ____ __ (score >= 80)
-            letter_grade = 'B';
-        ____ __ (score >= 70)
-            letter_grade = 'C';
-        ____ __ (score >=60)
-            letter_grade = 'D';
-        ____
-            letter_grade = 'F';
-            
-        c.. __ "Your grade is : " __ letter_grade __ e..
-        __ (letter_grade __ 'F')
-            c.. __ "Sorry, you must repeat the class" __ e..
-        ____
-            c.. __ "Congrats!" __ e..
-
-    } ____ {
-        c.. __ "Sorry, " __ score __ " is not in range" __ e..
-    }
-    
-    
-    c..  __ e..
-    r_ 0;
-}
-
+//// Section 9
+//// Grades
+//
+///*
+//    Calculate a sudent's grade on an exam given their score
+//    and tell them if they passed the course
+//
+//*/
+//
+//? ios..
+//
+//u.. s..
+//
+//in. main
+//
+//    in. score
+//    c.. __ "Enter your score on the exam (0-100) : "
+//    ci. __ score;
+//    ch.. letter_grade
+//
+//    __ s.. >_ 0 __ s.. <_100)
+//        __ (s.. >_ 90
+//            l... _ 'A'
+//        ____ __ (s.. >_ 80
+//            l... _ 'B'
+//        ____ __ (s.. >_ 70
+//            l... _ 'C'
+//        ____ __ (s.. >_60
+//            l... _ 'D'
+//        ____
+//            l... _ 'F'
+//
+//        c.. __ "Your grade is : " __ l... __ e..
+//        __ (l... __ 'F')
+//            c.. __ "Sorry, you must repeat the class" __ e..
+//        ____
+//            c.. __ "Congrats!" __ e..
+//
+//     ____
+//        c.. __ "Sorry, " __ s.. __ " is not in range" __ e..
+//
+//
+//
+//    c..  __ e..
+//    r_ _
+//
+//
