7470c398860443e25bc20c4055fd658274d4e673
Grey <sergejyurskyj@yahoo.com>
2020-07-17 12:06:57 -0700

    Committed 006_PassingPointers template

--- templates/033_Pointers/Section-12-Pointers-and-References/006_PassingPointers/main.cpp
+++ templates/033_Pointers/Section-12-Pointers-and-References/006_PassingPointers/main.cpp
@@ -1,27 +1,26 @@
-// Section 12
-// Passing Pointers 1
-? |i..
-
-u.. s..
-
-
-v.. double_data(in. *int_ptr) {
-   *int_ptr *_ 2
-}
-
-in. main
-    in. value {10}
-    in. *int_ptr {nullptr}
-    
-    c.. __ "Value: " __ value __ e..
-    double_data(&value)
-    c.. __ "Value: " __ value __ e..
-
-    c.. __ "-----------------------------" __ e..
-    int_ptr _ &value
-    double_data(int_ptr)
-    c.. __ "Value: " __ value __ e..
-
-    c..  __ e..
-    r_ _
-}
+//// Section 12
+//// Passing Pointers 1
+//? |i..
+//
+//u.. s..
+//
+//
+//v.. double_data in. #int_ptr
+//     #int_ptr *_ 2
+//
+//
+//in. main
+//    in. value 10
+//    in. #i_p.. n_p..
+//
+//    c.. __ "Value: " __ ? __ e..
+//    d.. #v..
+//    c.. __ "Value: " __ ? __ e..
+//
+//    c.. __ "-----------------------------" __ e..
+//    int_ptr _ #v..
+//    d.. i_p..
+//    c.. __ "Value: " __ ? __ e..
+//
+//    c..  __ e..
+//    r_ _
