57326fadea677971bcb2234f05c99625f44150df
Grey <sergejyurskyj@yahoo.com>
2020-07-16 19:05:48 -0700

    Committed ArraysAndFunctions template

--- templates/025_Funstions/Section-11-Functions/ArraysAndFunctions/main.cpp
+++ templates/025_Funstions/Section-11-Functions/ArraysAndFunctions/main.cpp
@@ -1,34 +1,34 @@
-// Section 11
-// Arrays and functions
-? ios..
-
-u.. s..
-
-v.. print_array(co.. in. arr[], size_t size);
-v.. set_array(in. arr[], size_t size, in. value);
-
-v.. print_array(co.. in. arr[], size_t size) {  // const
-    ___ (size_t i{0}; i < size; ++i)
-        c.. __ arr[i] __ " ";
-    c.. __ e..
-   // arr[0] = 50000; // bug
-}
-
-// set each array element to value
-v.. set_array(in. arr[], size_t size, in. value) {
-    ___ (size_t i{0}; i < size; ++i)
-        arr[i] = value;
-}
-
-in. main
-    in. my_scores[] {100, 98, 90, 86, 84};
-    
-    print_array(my_scores, 5);
-    set_array(my_scores, 5, 100);
-    print_array(my_scores, 5);
-    print_array(my_scores, 5);
-    
-    c.. __ e..
-    r_ _
-}
-
+//// Section 11
+//// Arrays and functions
+//? ios..
+//
+//u.. s..
+//
+//v.. print_array co.. in. arr|| size_t size
+//v.. set_array in. arr|| size_t size, in. value
+//
+//v.. ? co.. in. arr|| size_t size)   // const
+//    ___ s_t ? 0 ? < s.. ###
+//        c.. __ a.. ? __ " "
+//    c.. __ e..
+//   // arr[0] = 50000; // bug
+//
+//
+//// set each array element to value
+//v.. ?(in. arr|| size_t size, in. value
+//    ___ s_t ? 0 ? < s.. ###
+//        a.. ? _ v..
+//
+//
+//in. main
+//    in. my_scores|| 100, 98, 90, 86, 84
+//
+//    p_a.. m.. 5
+//    s_a.. m.. 5, 100
+//    p_a.. m.. 5
+//    p_a.. m.. 5
+//
+//    c.. __ e..
+//    r_ _
+//
+//
