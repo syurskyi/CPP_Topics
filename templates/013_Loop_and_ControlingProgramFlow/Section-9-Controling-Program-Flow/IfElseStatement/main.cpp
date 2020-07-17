499690fd557c54eac5c5b14c07a3f4e7dd541068
Grey <sergejyurskyj@yahoo.com>
2020-07-16 18:27:16 -0700

    Committed IfStatement template

--- templates/013_Loop_and_ControlingProgramFlow/Section-9-Controling-Program-Flow/IfElseStatement/main.cpp
+++ templates/013_Loop_and_ControlingProgramFlow/Section-9-Controling-Program-Flow/IfElseStatement/main.cpp
@@ -1,28 +1,28 @@
-// Section 9
-// If-Else Statement
-? ios..
-
-u.. s..
-
-in. main
-    in. num
-    co.. in. target 10
-
-    c.. __ "Enter a number  and I'll compare it to " __ ? __ ": "
-    ci. __ n..
-    
-    __ (n.. >_ t..
-        c.. __ "\n==================================" __ e..
-        c.. __ n.. __ " is greater than or equal to " __ t.. __ e..
-        in. diff  n.. - t..
-        c.. __ n.. __ " is " __ d.. __ " greater than " __ t.. __ e..
-     ____
-        c.. __ "\n==================================" __ e..
-        c.. __ n.. __ " is less than " __ t.. __ e..
-        in. d.. | t.. - n..
-        c.. __ n.. __ " is " __ d.. __ " less than " __ t.. __ e..
-
-    c..  __ e..
-    r_ _
-
-
+//// Section 9
+//// If-Else Statement
+//? ios..
+//
+//u.. s..
+//
+//in. main
+//    in. num
+//    co.. in. target 10
+//
+//    c.. __ "Enter a number  and I'll compare it to " __ ? __ ": "
+//    ci. __ n..
+//
+//    __ (n.. >_ t..
+//        c.. __ "\n==================================" __ e..
+//        c.. __ n.. __ " is greater than or equal to " __ t.. __ e..
+//        in. diff  n.. - t..
+//        c.. __ n.. __ " is " __ d.. __ " greater than " __ t.. __ e..
+//     ____
+//        c.. __ "\n==================================" __ e..
+//        c.. __ n.. __ " is less than " __ t.. __ e..
+//        in. d.. | t.. - n..
+//        c.. __ n.. __ " is " __ d.. __ " less than " __ t.. __ e..
+//
+//    c..  __ e..
+//    r_ _
+//
+//
--- templates/013_Loop_and_ControlingProgramFlow/Section-9-Controling-Program-Flow/IfStatement/main.cpp
+++ templates/013_Loop_and_ControlingProgramFlow/Section-9-Controling-Program-Flow/IfStatement/main.cpp
@@ -1,48 +1,48 @@
-// Section 9
-// If Statement 
-
-? ios..
-
-u.. s..
-
-in. main
-    in. num {};
-    co.. in. min {10};
-    co.. in. max {100};
-    
-    c.. __ "Enter a number between " __ min __ " and " __ max __ ": ";
-    ci. __ num;
-    
-    __ (num >= min) {
-        c.. __ "\n=========== If statement 1 =============" __ e..
-        c.. __ num __ " is greater than or equal to " __ min __ e..
-      
-        in. diff {num - min};
-        c.. __ num __ " is " __ diff __ " greater than " __ min __ e..
-    }
-    
-    __ (num <= max) {
-        c.. __ "\n=========== If statement 2=============" __ e..
-        c.. __ num __ " is less than or equal to " __ max __ e..
-        
-        in. diff { max -num};
-        c.. __ num __ " is " __ diff __ " less than " __ max __ e..
-    }
-    
-    __ (num >= min && num <=max) {
-      c.. __ "\n=========== If statement 3=============" __ e..
-      c.. __ num __ " is in range " __ e..
-      c.. __ " This means satements 1 and 2 must also display!!" __ e..
-    }
-    
-    __ (num __ min || num __ max) {
-        c.. __ "\n=========== If statement 4 =============" __ e..
-        c.. __ num __ " is right on a boundary " __ e..
-        c.. __ "This means all 4 statements display " __ e..
-
-    }
-    
-    c.. __ e..
-    r_ 0;
-}
-
+//// Section 9
+//// If Statement
+//
+//? ios..
+//
+//u.. s..
+//
+//in. main
+//    in. num
+//    co.. in. min 10
+//    co.. in. max 100}
+//
+//    c.. __ "Enter a number between " __ mi. __ " and " __ ma. __ ": "
+//    ci. __ n..
+//
+//    __ (n.. >_ mi.
+//        c.. __ "\n=========== If statement 1 =============" __ e..
+//        c.. __ n.. __ " is greater than or equal to " __ mi. __ e..
+//
+//        in. diff n.. - mi.
+//        c.. __ n.. __ " is " __ d.. __ " greater than " __ mi. __ e..
+//
+//
+//    __ (n.. <_ ma.
+//        c.. __ "\n=========== If statement 2=============" __ e..
+//        c.. __ n.. __ " is less than or equal to " __ ma. __ e..
+//
+//        in. d.. | ma. -n..
+//        c.. __ n.. __ " is " __ d.. __ " less than " __ ma. __ e..
+//
+//
+//    __ (n.. >_ mi. __ n.. <_ma.
+//      c.. __ "\n=========== If statement 3=============" __ e..
+//      c.. __ n.. __ " is in range " __ e..
+//      c.. __ " This means satements 1 and 2 must also display!!" __ e..
+//
+//
+//    __ (n.. __ mi. || n.. __ ma.
+//        c.. __ "\n=========== If statement 4 =============" __ e..
+//        c.. __ n.. __ " is right on a boundary " __ e..
+//        c.. __ "This means all 4 statements display " __ e..
+//
+//
+//
+//    c.. __ e..
+//    r_ _
+//
+//
