2f5be815d2fd0d651e805fc38a26e2a0a65e819b
Grey <sergejyurskyj@yahoo.com>
2020-07-17 11:14:56 -0700

    Committed PassByReference template

--- templates/025_Funstions/Section-11-Functions/PassByReference/main.cpp
+++ templates/025_Funstions/Section-11-Functions/PassByReference/main.cpp
@@ -1,60 +1,60 @@
-// Section 11
-// Pass-by-reference
-
-? ios..
-? st..
-? ve..
-
-u.. s..
-
-v.. pass_by_ref1(in. &num);
-v.. pass_by_ref2(string &s);
-v.. pass_by_ref3(vectorst.. &v);
-v.. print_vector(co.. vectorst.. &v);  // const
-
-v.. pass_by_ref1(in. &num) {
-    num = 1000;
-}
-
-v.. pass_by_ref2(string &s) {
-    s = "Changed";
-}
-
-v.. pass_by_ref3(vectorst.. &v) {
-    v.clear();  // delete all vector elements
-}
-
-v.. print_vector( co.. vectorst.. &v)  {
-    ___ (a.. s: v)
-        c.. __ s __ " ";
-    c.. __ e..
-}
-
-in. main
-    in. num {10};
-    in. another_num {20};
-    
-    c.. __ "num before calling pass_by_ref1: " __ num __ e..
-    pass_by_ref1(num);
-    c.. __ "num after calling pass_by_ref1: " __ num __ e..
-    
-    c.. __ "\nanother_num before calling pass_by_ref1: " __ another_num __ e..
-    pass_by_ref1(another_num);
-    c.. __ "another_num after calling pass_by_ref1: " __ another_num __ e..
-
-    string name {"Frank"};
-    c.. __ "\nname before calling pass_by_ref2: " __ name __ e..
-    pass_by_ref2(name);
-    c.. __ "name after calling pass_by_ref2: " __ name __ e..
-
-    vectorst.. stooges {"Larry", "Moe", "Curly"};
-    c.. __ "\nstooges before calling pass_by_ref3: ";
-    print_vector(stooges);
-    pass_by_ref3(stooges);
-    c.. __ "stooges after calling pass_by_ref3: ";
-    print_vector(stooges);
-    
-    c.. __ e..
-    r_ 0;
-}
-
+//// Section 11
+//// Pass-by-reference
+//
+//? ios..
+//? st..
+//? ve..
+//
+//u.. s..
+//
+//v.. pass_by_ref1 in. @num
+//v.. pass_by_ref2(st.. @s
+//v.. pass_by_ref3(vectorst.. @v
+//v.. print_vector(co.. vectorst.. @v  // const
+//
+//v.. ? in. @num
+//    ? _ 1000
+//
+//
+//v.. ? st.. @s
+//    s _ "Changed"
+//
+//
+//v.. ? vectorst.. @v
+//    ?.cl..  // delete all vector elements
+//
+//
+//v.. ? co.. vectorst.. @v
+//    ___ (a.. s: ?
+//        c.. __ s __ " "
+//    c.. __ e..
+//
+//
+//in. main
+//    in. num 10
+//    in. another_num 20
+//
+//    c.. __ "num before calling pass_by_ref1: " __ ? __ e..
+//    ? ?
+//    c.. __ "num after calling pass_by_ref1: " __ ? __ e..
+//
+//    c.. __ "\nanother_num before calling pass_by_ref1: " __ ? __ e..
+//    ? ?
+//    c.. __ "another_num after calling pass_by_ref1: " __ ? __ e..
+//
+//    string name {"Frank"};
+//    c.. __ "\nname before calling pass_by_ref2: " __ ? __ e..
+//    ? ?
+//    c.. __ "name after calling pass_by_ref2: " __ ? __ e..
+//
+//    vectorst.. stooges {"Larry", "Moe", "Curly"
+//    c.. __ "\nstooges before calling pass_by_ref3: "
+//    pr.. ?
+//    _3 ?
+//    c.. __ "stooges after calling pass_by_ref3: "
+//    pr.. ?
+//
+//    c.. __ e..
+//    r_ _
+//
+//
