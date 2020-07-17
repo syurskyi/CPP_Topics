31e6da730d7a84bb3848496429096ff0e22239ca
Grey <sergejyurskyj@yahoo.com>
2020-07-17 10:54:48 -0700

    Committed FunctionDefinitions template

--- templates/025_Funstions/Section-11-Functions/FunctionDefinitions/main.cpp
+++ templates/025_Funstions/Section-11-Functions/FunctionDefinitions/main.cpp
@@ -1,45 +1,44 @@
-// Section 11
-// Function Definitions
-// Area of Circle and Volume of a Cylinder
-? ios..
-
-u.. s..
-
-co.. do.. pi {3.14159};
-
-do.. calc_area_circle(do.. radius) {
-    r_ pi * radius * radius;
-}
-
-do.. calc_volume_cylinder(do.. radius, do.. height) {
-//    return pi * radius * radius * height;
-    r_ calc_area_circle(radius) * height;
-}
-
-v.. area_circle
-    do.. radius{};
-    c.. __ "\nEnter the radius of the circle: ";
-    ci. __ radius;
-    c.. __ "The area of a circle with radius " __ radius __ " is " __ calc_area_circle(radius) __ e..
-}
-
-v.. volume_cylinder
-    do.. radius {};
-    do.. height {};
-    c.. __ "\nEnter the radius of the cylinder: ";
-    ci. __ radius;
-    c.. __ "\nEnter the height of the cylinder: ";
-    ci. __ height;
-    
-    c.. __ "The volume of a cylinder with radius " __ radius __ " and height " __ height __ " is " __ calc_volume_cylinder(radius, height) __ e..
-}
-
-in. main
-    
-    area_circle();
-    area_circle();
-    volume_cylinder();
-    
-    r_ 0;
-}
-
+//// Section 11
+//// Function Definitions
+//// Area of Circle and Volume of a Cylinder
+//? ios..
+//
+//u.. s..
+//
+//co.. do.. pi 3.14159
+//
+//do.. calc_area_circle do.. radius
+//    r_ p. * r.. * r..
+//
+//
+//do.. calc_volume_cylinder do.. radius, do.. height
+////    return pi * radius * radius * height;
+//    r_ c_a_c..(r.. * h..
+//
+//
+//v.. area_circle
+//    do.. r..
+//    c.. __ "\nEnter the radius of the circle: "
+//    ci. __ ?
+//    c.. __ "The area of a circle with radius " __ ? __ " is " __ c_a_c.. ? __ e..
+//
+//
+//v.. volume_cylinder
+//    do.. r..
+//    do.. h..
+//    c.. __ "\nEnter the radius of the cylinder: "
+//    ci. __ ?
+//    c.. __ "\nEnter the height of the cylinder: "
+//    ci. __ ?
+//
+//    c.. __ "The volume of a cylinder with radius " __ ? __ " and height " __ h.. __ " is " __ c_v_c.. r.. h.. __ e..
+//
+//
+//in. main
+//
+//    a..
+//    a..
+//    v..
+//
+//    r_ _
+//
