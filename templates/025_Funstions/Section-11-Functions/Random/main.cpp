7a85f299d9d9885e2b3dabf5d101319659e779ec
Grey <sergejyurskyj@yahoo.com>
2020-07-17 11:20:04 -0700

    Committed ScopeExample template

--- templates/025_Funstions/Section-11-Functions/Random/main.cpp
+++ templates/025_Funstions/Section-11-Functions/Random/main.cpp
@@ -1,31 +1,31 @@
-// Section 11
-// Random Numbers
-
-? ios..
-? <cstdlib>      // required for rand()
-? <ctime>       // required for time( )
-
-u.. s..
-
-in. main
-      
-    in. random_number {};
-    size_t count {10};           // number of random numbers to generate
-    in. min {1};                  // lower bound (inclusive)
-    in. max {6};                 // upper bound (inclusive)
-    
-    // seed the random number generator
-    // If you don't seed the generator you will get the same requence random numbers every run
-    
-    c.. __ "RAND_MAX on my system is: " __ RAND_MAX __ e..
-    srand(time(nullptr));  
-    
-    ___ (size_t i{1}; i<=count; ++i)  {
-        random_number = rand() % max + min;     // generate a random number [min, max]
-        c.. __ random_number __ e..
-    }
-
-    c..  __ e..
-    r_ 0;
-}
-
+//// Section 11
+//// Random Numbers
+//
+//? ios..
+//? cst..      // required for rand()
+//? ct..       // required for time( )
+//
+//u.. s..
+//
+//in. main
+//
+//    in. random_number
+//    size_t count 10           // number of random numbers to generate
+//    in. min 1                 // lower bound (inclusive)
+//    in. max 6                // upper bound (inclusive)
+//
+//    // seed the random number generator
+//    // If you don't seed the generator you will get the same requence random numbers every run
+//
+//    c.. __ "RAND_MAX on my system is: " __ ? __ e..
+//    sr.. ti.. nullptr
+//
+//    ___  s_t i 1 ?<_c.. ###
+//        random_number = ra.. % ma. + mi.    // generate a random number [min, max]
+//        c.. __ ? __ e..
+//
+//
+//    c..  __ e..
+//    r_ _
+//
+//
