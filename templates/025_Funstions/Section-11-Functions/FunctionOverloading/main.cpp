d2937086adb9c4ea21745efa15edef0a2f07e975
Grey <sergejyurskyj@yahoo.com>
2020-07-17 11:02:05 -0700

    Committed FunctionParamters template

--- templates/025_Funstions/Section-11-Functions/FunctionOverloading/main.cpp
+++ templates/025_Funstions/Section-11-Functions/FunctionOverloading/main.cpp
@@ -1,58 +1,58 @@
-// Section 11
-// Function Overloading
-? ios..
-? st..
-? ve..
-
-u.. s..
-
-v.. print(in.
-v.. print(do..
-v.. print(string
-v.. print(string, string
-v.. print(vectorst..
-
-v.. print(in. num
-    c.. __ "Printing int: " __ ? __ e..
-
-
-v.. print(do.. num
-    c.. __ "Printing double: " __ ? __ e..
-
-
-v.. print(string s
-    c.. __ "Printing string: " __ s __ e..
-
-
-v.. print(string s, string t
-    c.. __ "Printing 2 strings: " __ ? __ " and " __ ? __ e..
-
-
-v.. print(vectorst.. v
-    c.. __ "Printing vector of strings: "
-    ___ (a.. s: v
-        c.. __ s + " "
-    c.. __ e..
-
-
-in. main
-    print(100);    // int
-    print('A');     //  character is always promoted to int should print 65 ASCII ('A')
-    
-    print(123.5);   // double
-    print(123.3F);  // Float is promoted to double
-    
-    print("C-style string");  // C-style string is converted to a C++ string
-    
-    string s {"C++ string"};    
-    print(s);       // C++ string
-    
-    print("C-style string", s); // First argument is converted to a C++ string
-    
-    vectorst.. three_stooges {"Larry", "Moe", "Curly"};
-    print(three_stooges);
-    
-    c.. __ e..
-    r_ _
-}
-
+//// Section 11
+//// Function Overloading
+//? ios..
+//? st..
+//? ve..
+//
+//u.. s..
+//
+//v.. print(in.
+//v.. print(do..
+//v.. print(string
+//v.. print(string, string
+//v.. print(vectorst..
+//
+//v.. print(in. num
+//    c.. __ "Printing int: " __ ? __ e..
+//
+//
+//v.. print(do.. num
+//    c.. __ "Printing double: " __ ? __ e..
+//
+//
+//v.. print(string s
+//    c.. __ "Printing string: " __ s __ e..
+//
+//
+//v.. print(string s, string t
+//    c.. __ "Printing 2 strings: " __ ? __ " and " __ ? __ e..
+//
+//
+//v.. print(vectorst.. v
+//    c.. __ "Printing vector of strings: "
+//    ___ (a.. s: v
+//        c.. __ s + " "
+//    c.. __ e..
+//
+//
+//in. main
+//    print(100);    // int
+//    print('A');     //  character is always promoted to int should print 65 ASCII ('A')
+//
+//    print(123.5);   // double
+//    print(123.3F);  // Float is promoted to double
+//
+//    print("C-style string");  // C-style string is converted to a C++ string
+//
+//    string s {"C++ string"};
+//    print(s);       // C++ string
+//
+//    print("C-style string", s); // First argument is converted to a C++ string
+//
+//    vectorst.. three_stooges {"Larry", "Moe", "Curly"};
+//    print(three_stooges);
+//
+//    c.. __ e..
+//    r_ _
+//}
+//
--- templates/025_Funstions/Section-11-Functions/FunctionParamters/main.cpp
+++ templates/025_Funstions/Section-11-Functions/FunctionParamters/main.cpp
@@ -1,59 +1,58 @@
-// Section 11
-// Function Parmameters
-? ios..
-? st..
-? ve..
-
-u.. s..
-
-v.. pass_by_value1(in. num);
-v.. pass_by_value2(string s);
-v.. pass_by_value3(vectorst.. v);
-v.. print_vector(vectorst.. v);
-
-v.. pass_by_value1(in. num) {
-    num = 1000;
-}
-
-v.. pass_by_value2(string s) {
-    s = "Changed";
-}
-
-v.. pass_by_value3(vectorst.. v) {
-    v.clear();  // delete all vector elements
-}
-
-v.. print_vector(vectorst.. v) {
-    ___ (a.. s: v)
-        c.. __ s __ " ";
-    c.. __ e..
-}
-
-in. main
-    in. num {10};
-    in. another_num {20};
-    
-    c.. __ "num before calling pass_by_value1: " __ num __ e..
-    pass_by_value1(num);
-    c.. __ "num after calling pass_by_value1: " __ num __ e..
-    
-    c.. __ "\nanother_num before calling pass_by_value1: " __ another_num __ e..
-    pass_by_value1(another_num);
-    c.. __ "another_num after calling pass_by_value1: " __ another_num __ e..
-
-    string name {"Frank"};
-    c.. __ "\nname before calling pass_by_value2: " __ name __ e..
-    pass_by_value2(name);
-    c.. __ "name after calling pass_by_value2: " __ name __ e..
-
-    vectorst.. stooges {"Larry", "Moe", "Curly"};
-    c.. __ "\nstooges before calling pass_by_value3: ";
-    print_vector(stooges);
-    pass_by_value3(stooges);
-    c.. __ "stooges after calling pass_by_value3: ";
-    print_vector(stooges);
-    
-    c.. __ e..
-    r_ 0;
-}
-
+//// Section 11
+//// Function Parmameters
+//? ios..
+//? st..
+//? ve..
+//
+//u.. s..
+//
+//v.. pass_by_value1 in. num
+//v.. pass_by_value2 string s
+//v.. pass_by_value3 vectorst.. v
+//v.. print_vector vectorst.. v
+//
+//v.. ? in. num
+//    ? _ 1000
+//
+//
+//v.. ? string s
+//    s _ "Changed"
+//
+//
+//v.. ? vectorst.. v
+//    v.cl..  // delete all vector elements
+//
+//
+//v.. ?(vectorst.. v
+//    ___ (a.. s: ?
+//        c.. __ s __ " "
+//    c.. __ e..
+//
+//
+//in. main
+//    in. num {10
+//    in. another_num {20
+//
+//    c.. __ "num before calling pass_by_value1: " __ ? __ e..
+//    _1 ?
+//    c.. __ "num after calling pass_by_value1: " __ ? __ e..
+//
+//    c.. __ "\nanother_num before calling pass_by_value1: " __ a.. __ e..
+//    _1 ?
+//    c.. __ "another_num after calling pass_by_value1: " __ ? __ e..
+//
+//    string name {"Frank"};
+//    c.. __ "\nname before calling pass_by_value2: " __ ? __ e..
+//    ? ?
+//    c.. __ "name after calling pass_by_value2: " __ ? __ e..
+//
+//    vectorst.. stooges "Larry", "Moe", "Curly"
+//    c.. __ "\nstooges before calling pass_by_value3: "
+//    p.. ?
+//    _3 ?
+//    c.. __ "stooges after calling pass_by_value3: "
+//    pr.. ?
+//
+//    c.. __ e..
+//    r_ _
+//
