5286c328e6a9696d4c97717818611f8b7c969104
Grey <sergejyurskyj@yahoo.com>
2020-07-17 10:49:57 -0700

    Committed Fibonacci template

--- templates/025_Funstions/Section-11-Functions/Fibonacci/main.cpp
+++ templates/025_Funstions/Section-11-Functions/Fibonacci/main.cpp
@@ -1,25 +1,24 @@
-// Section 11
-// Recursion - Fibonacci
-? ios..
-
-u.. s..
-// Section 11
-// Recursion - Fibonacci
-
-? ios..
-
-u.. s..
-u.. l.. l.. fibonacci(u.. l.. l.. n);
-
-u.. l.. l.. fibonacci(u.. l.. l.. n) {
-    __ (n <= 1)
-        r_ n;	             // base cases
-    r_ fibonacci(n-1) + fibonacci(n-2); // recursion
-}
-
-in. main
-    c.. __ fibonacci(5) __ e..   // 5
-    c.. __ fibonacci(30) __ e..	// 832040 
-    c.. __ fibonacci(40) __ e.. // 102334155
-    r_ 0;
-}
+//// Section 11
+//// Recursion - Fibonacci
+//? ios..
+//
+//u.. s..
+//// Section 11
+//// Recursion - Fibonacci
+//
+//? ios..
+//
+//u.. s..
+//u.. l.. l.. fibonacci u.. l.. l.. n
+//
+//u.. l.. l.. ? u.. l.. l.. n
+//    __ (n <_ 1
+//        r_ ?	             // base cases
+//    r_ ? ?-1) + ? ?-2) // recursion
+//
+//
+//in. main
+//    c.. __ ? 5 __ e..   // 5
+//    c.. __ ? 30 __ e..	// 832040 
+//    c.. __ ? 40 __ e.. // 102334155
+//    r_ _
