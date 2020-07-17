66fb750eb1ef190278f616af9735192497061505
Grey <sergejyurskyj@yahoo.com>
2020-07-17 11:09:50 -0700

    Committed Math template

--- templates/025_Funstions/Section-11-Functions/Math/main.cpp
+++ templates/025_Funstions/Section-11-Functions/Math/main.cpp
@@ -1,36 +1,35 @@
-// Section 11
-// Math examples
-
-? ios..
-? <cmath>        // required
-
-u.. s..
-
-in. main
-    
-    do.. num {};
-
-    c.. __ "Enter a number (double) : ";
-    ci. __ num;
-    
-    c.. __ "The sqrt of " __ num __ " is: " __ sqrt(num) __ e..
-    c.. __ "The cubed root of " __ num __ " is: " __ cbrt(num) __ e..
-    
-    c.. __ "The sine of " __ num __ " is: " __ sin(num) __ e..
-    c.. __ "The cosine of " __ num __ " is: " __ cos(num) __ e..
-    
-    c.. __ "The ceil of " __ num __ " is: " __ ceil(num) __ e..
-    c.. __ "The floor of " __ num __ " is: " __ floor(num) __ e..
-    c.. __ "The round of " __ num __ " is: " __ round(num) __ e..
-
-    
-    do.. power {};
-    c.. __ "\nEnter a power to raise " __ num __ " to: ";
-    ci. __ power;
-    c.. __ num __ " raised to the " __ power __ " power is: " __ pow(num, power) __ e..
-    
-
-    c.. __  e..
-    r_ 0;
-}
-
+//// Section 11
+//// Math examples
+//
+//? ios..
+//? <cmath>        // required
+//
+//u.. s..
+//
+//in. main
+//
+//    do.. num
+//
+//    c.. __ "Enter a number (double) : "
+//    ci. __ ?
+//
+//    c.. __ "The sqrt of " __ ? __ " is: " __ sq.. ? __ e..
+//    c.. __ "The cubed root of " __ ? __ " is: " __ cb.. ? __ e..
+//
+//    c.. __ "The sine of " __ ? __ " is: " __ si. ? __ e..
+//    c.. __ "The cosine of " __ ? __ " is: " __ co. ? __ e..
+//
+//    c.. __ "The ceil of " __ ? __ " is: " __ ce.. ? __ e..
+//    c.. __ "The floor of " __ ? __ " is: " __ fl.. ? __ e..
+//    c.. __ "The round of " __ ? __ " is: " __ ro.. ? __ e..
+//
+//
+//    do.. power
+//    c.. __ "\nEnter a power to raise " __ ? __ " to: "
+//    ci. __ ?
+//    c.. __ ? __ " raised to the " __ ? __ " power is: " __ po. ?, ? __ e..
+//
+//
+//    c.. __  e..
+//    r_ _
+//
