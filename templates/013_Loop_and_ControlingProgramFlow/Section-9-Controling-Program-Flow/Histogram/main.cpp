5365898b20922b98d9c0d6f195ac7e43d3439b0f
Grey <sergejyurskyj@yahoo.com>
2020-07-16 18:20:07 -0700

    Committed Histogram template

--- templates/013_Loop_and_ControlingProgramFlow/Section-9-Controling-Program-Flow/Histogram/main.cpp
+++ templates/013_Loop_and_ControlingProgramFlow/Section-9-Controling-Program-Flow/Histogram/main.cpp
@@ -1,39 +1,39 @@
-// Section 9
-// Nested Loops - Histogram
-
-? ios..
-? ve..
-
-u.. s..
-
-in. main
-    
-    in. num_items{};
-    
-    c.. __ "How many data items do you have? ";
-    ci. __ num_items;
-    
-    vector<in.> data {};
-    
-    ___ (in. i{1}; i<= num_items; ++i) {
-        in. data_item{};
-        c.. __ "Enter data item " __ i __ ": ";
-        ci. __ data_item;
-        data.push_back(data_item);
-    }
-    
-    c.. __ "\nDisplaying Histogram" __ e..
-    ___ (auto val: data) {
-        ___ (in. i{1} ; i<=val; ++i) {
-            __ (i % 5 __ 0)
-                c.. __ "*";
-            ____
-                c.. __ "-";
-        }    
-        c.. __ e..
-    }
-
-    c.. __  e..
-    r_ 0;
-}
-
+//// Section 9
+//// Nested Loops - Histogram
+//
+//? ios..
+//? ve..
+//
+//u.. s..
+//
+//in. main
+//
+//    in. num_items
+//
+//    c.. __ "How many data items do you have? "
+//    ci. __ ?
+//
+//    ve..|in.| data
+//
+//    ___ (in. i|1 ?<_ ? ###
+//        in. data_item
+//        c.. __ "Enter data item " __ i __ ": "
+//        ci. __ ?
+//        ?.p_b.. ?
+//
+//
+//    c.. __ "\nDisplaying Histogram" __ e..
+//    ___ a.. val: d..
+//        ___ in. i 1  ?<_? ###
+//            __ ? % 5 __ 0
+//                c.. __ "*"
+//            ____
+//                c.. __ "-"
+//        }
+//        c.. __ e..
+//
+//
+//    c.. __  e..
+//    r_ _
+//
+//
