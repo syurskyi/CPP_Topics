e9bd7d2b47b1853365a27df3233eb445c3ec958b
Grey <sergejyurskyj@yahoo.com>
2020-07-18 13:19:06 -0700

    Committed ShallowCopy template

--- templates/040_Classes_and_Objects/Beginning_CPP_Programming_From_Beginner_to_Beyond/Section-13-OOPClasses-and-Objects/ShallowCopy/main.cpp
+++ templates/040_Classes_and_Objects/Beginning_CPP_Programming_From_Beginner_to_Beyond/Section-13-OOPClasses-and-Objects/ShallowCopy/main.cpp
@@ -1,51 +1,50 @@
-// Section 13
-// Copy Constructor - Shallow Copy
-// Note: this program will crash
-? ios..
-
-u.. s..
-
-c_ Shallow {
-pr..
-    in. *data;
-pu..
-    v.. set_data_value(in. d) { *data = d; }
-    in. get_data_value r_ *data; }
-    // Constructor
-    Shallow(in. d);
-    // Copy Constructor
-    Shallow(co.. Shallow &source);
-    // Destructor
-    ~Shallow();
-};
-
-Shallow::Shallow(in. d) {
-    data = ne. in.;
-    *data = d;
-}
-
-Shallow::Shallow(co.. Shallow &source)
-    : data(source.data) {
-        c.. __ "Copy constructor  - shallow copy" __ e..
-}
-
-Shallow::~Shallow
-    de... data;
-    c.. __ "Destructor freeing data" __ e..
-}
-
-v.. display_shallow(Shallow s) {
-    c.. __ s.get_data_value() __ e..
-}
-
-in. main
-    
-    Shallow obj1 {100};
-    display_shallow(obj1);
-    
-    Shallow obj2 {obj1};
-    obj2.set_data_value(1000);
-    
-    r_ 0;
-}
-
+//// Section 13
+//// Copy Constructor - Shallow Copy
+//// Note: this program will crash
+//? ios..
+//
+//u.. s..
+//
+//c_ Shallow
+//pr..
+//    in. #data
+//pu..
+//    v.. set_data_value in. d  #data _ d
+//    in. get_data_value r_ #d..
+//    // Constructor
+//    ? in. d
+//    // Copy Constructor
+//    ? co.. ? #source
+//    // Destructor
+//    #?
+//
+//
+//? ? in. d
+//    data _ ne. in.
+//    #d.. _ d
+//
+//
+//? ? co.. ? #source
+//    | data ?.d..
+//        c.. __ "Copy constructor  - shallow copy" __ e..
+//
+//
+//? #?
+//    de... data;
+//    c.. __ "Destructor freeing data" __ e..
+//
+//
+//v.. display_shallow ? s
+//    c.. __ ?.g_d_v.. __ e..
+//
+//
+//in. main
+//
+//    ? obj1 100
+//    d_s.. ?
+//
+//    ? obj2 ?
+//    ?.s_d_v.. 1000
+//
+//    r_ _
+//
