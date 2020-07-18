6c515f39a3b2c7efc83e137412792402861e2f52
Grey <sergejyurskyj@yahoo.com>
2020-07-18 13:09:38 -0700

    Committed MoveConstructor template

--- templates/040_Classes_and_Objects/Beginning_CPP_Programming_From_Beginner_to_Beyond/Section-13-OOPClasses-and-Objects/MoveConstructor/main.cpp
+++ templates/040_Classes_and_Objects/Beginning_CPP_Programming_From_Beginner_to_Beyond/Section-13-OOPClasses-and-Objects/MoveConstructor/main.cpp
@@ -1,68 +1,68 @@
-// Section 13
-// Move Constructor 
-? ios..
-? ve..
-
-u.. s..
-
-c_ Move
-pr..
-    in. *data;
-pu..
-    v.. set_data_value(in. d) { *data = d; }
-    in. get_data_value r_ *data; }
-    // Constructor
-    Move(in. d);
-    // Copy Constructor
-    Move(co.. Move &source);
-    // Move Constructor
-    Move(Move &&source) noexcept;
-    // Destructor
-    ~Move();
-
-
- Move::Move(in. d)  {
-    data = ne. in.;
-    *data = d;
-    c.. __ "Constructor for: " __ d __ e..
-
-
-// Copy ctor
-Move::Move(co.. Move &source)
-    : Move {*source.data} {
-        c.. __ "Copy constructor  - deep copy for: " __ *data __ e..
-
-
-//Move ctor
-Move::Move(Move &&source) noexcept 
-    : data {source.data} {
-        source.data = n_p_;
-        c.. __ "Move constructor - moving resource: " __ *data __ e..
-
-
-Move::~Move
-    __ (data != n_p_) {
-        c.. __ "Destructor freeing data for: " __ *data __ e..
-    } ____ {
-        c.. __ "Destructor freeing data for nullptr" __ e..
-    }
-    de... data;
-}
-
-in. main
-    ve..<Move> vec;
-
-    vec.push_back(Move{10});
-
-    vec.push_back(Move{20});
-    vec.push_back(Move{30});
-    vec.push_back(Move{40});
-     vec.push_back(Move{50});
-    vec.push_back(Move{60});
-    vec.push_back(Move{70});
-    vec.push_back(Move{80});
-
-
-    r_ 0;
-}
-
+//// Section 13
+//// Move Constructor
+//? ios..
+//? ve..
+//
+//u.. s..
+//
+//c_ Move
+//pr..
+//    in. #data
+//pu..
+//    v.. set_data_value in. d #data _ d
+//    in. get_data_value r_ #data
+//    // Constructor
+//    ? in. d
+//    // Copy Constructor
+//    ? co.. ? #source
+//    // Move Constructor
+//    ? ? ##s.. noe..
+//    // Destructor
+//    #?
+//
+//
+// ? ? in. d
+//    data _ ne. in.
+//    #d.. _ ?
+//    c.. __ "Constructor for: " __ ? __ e..
+//
+//
+//// Copy ctor
+//? ? co.. ? #s..
+//    : ? #s__.d..
+//        c.. __ "Copy constructor  - deep copy for: " __ #d.. __ e..
+//
+//
+////Move ctor
+//? ? ? ##s.. noe..
+//    | data s__.d..
+//        s__.d.. _ n_p_
+//        c.. __ "Move constructor - moving resource: " __ #d.. __ e..
+//
+//
+//?::~?
+//    __ (data !_ n_p_) {
+//        c.. __ "Destructor freeing data for: " __ *data __ e..
+//    } ____ {
+//        c.. __ "Destructor freeing data for nullptr" __ e..
+//    }
+//    de... data;
+//}
+//
+//in. main
+//    ve..<?> vec;
+//
+//    vec.push_back(?{10});
+//
+//    vec.push_back(?{20});
+//    vec.push_back(?{30});
+//    vec.push_back(?{40});
+//     vec.push_back(?{50});
+//    vec.push_back(?{60});
+//    vec.push_back(?{70});
+//    vec.push_back(?{80});
+//
+//
+//    r_ 0;
+//}
+//
