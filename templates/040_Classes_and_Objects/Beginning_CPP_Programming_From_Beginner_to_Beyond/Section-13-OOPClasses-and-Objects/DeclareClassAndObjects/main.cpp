653025af220ec6fd021c5f32728709c4adcab8ec
Grey <sergejyurskyj@yahoo.com>
2020-07-18 12:20:06 -0700

    Committed DeclareClassAndObjects template

--- templates/040_Classes_and_Objects/Beginning_CPP_Programming_From_Beginner_to_Beyond/Section-13-OOPClasses-and-Objects/DeclareClassAndObjects/main.cpp
+++ templates/040_Classes_and_Objects/Beginning_CPP_Programming_From_Beginner_to_Beyond/Section-13-OOPClasses-and-Objects/DeclareClassAndObjects/main.cpp
@@ -1,52 +1,51 @@
-// Section 13
-// Declare Classes and Objects
-? ios..
-? st..
-? ve..
-
-u.. s..
-
-c_ Player {
-    // attributes
-    st..  name {"Player"};
-    in. health {100};
-    in. xp {3};
-    
-    // methods
-    v.. talk(st.. );
-    b.. is_dead();
-};
-
-c_ Account {
-    // attributes
-    st..  name {"Account"};
-    do.. balance {0.0};
-    
-    // methods
-    b.. deposit(do..);
-    b.. withdraw(do..);
-    
-};
-
-
-in. main
-    Account frank_account;
-    Account jim_account;
-    
-    
-    Player frank;
-    Player hero;
-    
-    Player players[] {frank, hero};
-    
-    ve..<Player> player_vec {frank};
-    player_vec.push_back(hero);
-    
-    
-    Player *enemy {n_p_};
-    enemy = ne. Player;
-    
-    de... enemy;
-
-    r_ 0;
-}
+//// Section 13
+//// Declare Classes and Objects
+//? ios..
+//? st..
+//? ve..
+//
+//u.. s..
+//
+//c_ Player
+//    // attributes
+//    st..  name "Player"
+//    in. health 100
+//    in. xp 3
+//
+//    // methods
+//    v.. talk st..
+//    b.. is_dead
+//
+//
+//c_ Account
+//    // attributes
+//    st..  name "Account"
+//    do.. balance 0.0
+//
+//    // methods
+//    b.. deposit(do..
+//    b.. withdraw(do..
+//
+//
+//
+//
+//in. main
+//    A.. frank_account
+//    A.. jim_account
+//
+//
+//    P.. frank
+//    P.. hero
+//
+//    P.. players|| frank, hero
+//
+//    ve..|P..| player_vec frank
+//    p_v_.p_b.. h..
+//
+//
+//    P.. #e.. n_p_
+//    enemy _ ne. P..
+//
+//    de... ?
+//
+//    r_ _
