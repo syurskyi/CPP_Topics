a65b81be43267f841f69ee316b2d154dacfa2458
Grey <sergejyurskyj@yahoo.com>
2020-07-18 13:23:04 -0700

    Committed StaticClassMembers template

--- templates/040_Classes_and_Objects/Beginning_CPP_Programming_From_Beginner_to_Beyond/Section-13-OOPClasses-and-Objects/StaticClassMembers/main.cpp
+++ templates/040_Classes_and_Objects/Beginning_CPP_Programming_From_Beginner_to_Beyond/Section-13-OOPClasses-and-Objects/StaticClassMembers/main.cpp
@@ -1,32 +1,32 @@
-// Section 13
-// Static class members
-? ios..
-? "Player.h"
-
-u.. s..
-
-v.. display_active_players
-    c.. __ "Active players: " __ Player::get_num_players() __ e..
-}
-
-in. main
-    display_active_players();
-    Player hero{"Hero"};
-    display_active_players();
-    
-    {
-        Player frank{"Frank"};
-        display_active_players();
-    }
-    display_active_players();
-    
-    Player *enemy = ne. Player("Enemy", 100, 100);
-    display_active_players();
-    de... enemy;
-    display_active_players();    
-
-
-
-    r_ 0;
-}
-
+//// Section 13
+//// Static class members
+//? ios..
+//? "P..
+//
+//u.. s..
+//
+//v.. display_active_players
+//    c.. __ "Active players: " __ ? g_n_p.. __ e..
+//
+//
+//in. main
+//    di_a_p..
+//    ? hero "Hero"
+//    d..
+//
+//
+//        ? frank "Frank"
+//        d..
+//
+//    d..
+//
+//    ? #en = ne. ? "Enemy", 100, 100
+//    d..
+//    de... enemy
+//    d..0
+//
+//
+//
+//    r_ _
+//
+//
