f9a7af784ac24d28a923504d0fa1681c7338919b
Grey <sergejyurskyj@yahoo.com>
2020-07-18 08:18:07 -0700

    Committed ConstInClasses template

--- templates/040_Classes_and_Objects/Beginning_CPP_Programming_From_Beginner_to_Beyond/Section-13-OOPClasses-and-Objects/ConstInClasses/main.cpp
+++ templates/040_Classes_and_Objects/Beginning_CPP_Programming_From_Beginner_to_Beyond/Section-13-OOPClasses-and-Objects/ConstInClasses/main.cpp
@@ -1,59 +1,58 @@
-// Section 13
-// Const in Classes
-? ios..
-? st..
-
-u.. s..
-
-c_ Player
-{
-pr..
-    st. st..  name;
-    in. health;
-     in. xp;
-pu..
-    st. st..  get_name() co..  {         // consty method
-        r_ name;
-    }
-    v.. set_name(st. st..  name_val)   {
-        name = name_val;
-    }
-// Overloaded Constructors
-    Player();
-    Player(st. st..  name_val);
-    Player(st. st..  name_val, in. health_val, in. xp_val);
-};
-
-Player::Player() 
-    : Player {"None",0,0} {
-}
-
-Player::Player(st. st..  name_val)
-   : Player {name_val,0, 0}  {
-}
-  
-Player::Player(st. st..  name_val, in. health_val, in. xp_val)
-    : name{name_val}, health{health_val}, xp{xp_val} {
-}
-
-v.. display_player_name(co.. Player &p) {
-    c.. __ p.get_name() __ e..
-}
-
-in. main
-    
-    co.. Player villain {"Villain", 100, 55};
-    Player hero {"Hero", 100, 15};
-    
-   // villain.set_name("Super villain");
-    c.. __ villain.get_name() __ e..
-    c.. __ hero.get_name() __ e..
-    
-    display_player_name(villain);
-    display_player_name(hero);
-
-    
-  
-    r_ 0;
-}
+//// Section 13
+//// Const in Classes
+//? ios..
+//? st..
+//
+//u.. s..
+//
+//c_ Player
+//
+//pr..
+//    st. st..  name
+//    in. health
+//     in. xp
+//pu..
+//    st. st..  get_name() co..          // consty method
+//        r_ n..
+//
+//    v.. set_name st. st..  name_val
+//        name _ n_v..
+//
+//// Overloaded Constructors
+//    ?
+//    ? st. st..  n_v..
+//    ? st. st..  n_v.. in. health_val, in. xp_val
+//
+//
+//? ?
+//    | ? "None",0,0
+//
+//
+//? ? st. st..  name_val
+//   | ? ?, 0, 0
+//
+//
+//? ? st. st..  name_val, in. health_val, in. xp_val
+//    | name ? health ?, xp ?
+//
+//
+//v.. display_player_name co.. ? #p
+//    c.. __ ?.g_n.. __ e..
+//
+//
+//in. main
+//
+//    co.. ? villain "Villain", 100, 55
+//    ? hero "Hero", 100, 15;
+//
+//   // villain.set_name("Super villain");
+//    c.. __ v__.g.. __ e..
+//    c.. __ h__.g.. __ e..
+//
+//    d.. v..
+//    d.. h..
+//
+//
+//
+//    r_ _
 
