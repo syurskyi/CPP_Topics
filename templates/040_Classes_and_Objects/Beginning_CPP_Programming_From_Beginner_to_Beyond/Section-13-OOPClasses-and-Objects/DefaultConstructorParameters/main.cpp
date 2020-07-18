037458af48370c0cd5614bd033e139088eaa4c6b
Grey <sergejyurskyj@yahoo.com>
2020-07-18 12:29:42 -0700

    Committed DefaultConstructorParameters template

--- templates/040_Classes_and_Objects/Beginning_CPP_Programming_From_Beginner_to_Beyond/Section-13-OOPClasses-and-Objects/DefaultConstructorParameters/main.cpp
+++ templates/040_Classes_and_Objects/Beginning_CPP_Programming_From_Beginner_to_Beyond/Section-13-OOPClasses-and-Objects/DefaultConstructorParameters/main.cpp
@@ -1,35 +1,35 @@
-// Section 13
-// Default Constructor Parameters
-? ios..
-? st..
-
-u.. s..
-
-c_ Player
-{
-pr..
-   st. st..  name;
-   in. health;
-   in. xp;
-pu..
-    Player(st. st..  name_val ="None", in. health_val = 0, in. xp_val = 0);
-  //  Player() {}    // Will cause a compiler error
-};
-
-  
-Player::Player(st. st..  name_val, in. health_val, in. xp_val)
-    : name{name_val}, health{health_val}, xp{xp_val} {
-            c.. __ "Three-args constructor" __ e..
-}
-
-
-in. main
-    
-    Player empty;
-    Player frank {"Frank"};
-    Player hero {"Hero", 100};
-    Player villain {"Villain", 100, 55};
-    
-    r_ 0;
-}
-
+//// Section 13
+//// Default Constructor Parameters
+//? ios..
+//? st..
+//
+//u.. s..
+//
+//c_ Player
+//
+//pr..
+//   st. st..  name
+//   in. health
+//   in. xp
+//pu..
+//    ? st. st..  name_val _"None", in. health_val _ 0, in. xp_val _ 0
+//  //  Player() {}    // Will cause a compiler error
+//
+//
+//
+//? ? st. st..  name_val, in. health_val, in. xp_val
+//    | n.. ?  h.. ?  x. ?
+//            c.. __ "Three-args constructor" __ e..
+//
+//
+//
+//in. main
+//
+//    ? empty
+//    ? frank  "Frank"
+//    ? hero  "Hero", 100
+//    ? villain  "Villain", 100, 55
+//
+//    r_ _
+//
+//
