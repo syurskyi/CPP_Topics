9a04d4ac9838864d1fe88ef5f45aba8fca4b2371
Grey <sergejyurskyj@yahoo.com>
2020-07-18 15:36:05 -0700

    Committed ConstructorsAndDestructors template

---
+++ templates/040_Classes_and_Objects/Beginning_CPP_Programming_From_Beginner_to_Beyond/Section-15-Inheritance/ConstructorsAndDestructors/main.cpp
@@ -0,0 +1,32 @@
+//// Section 15
+//// Constructors and Destructors
+//? ios..
+//
+//u.. s..
+//
+//c_ Base
+//pr..
+//    in. value
+//pu..
+//   ?  value 0  c.. __ "Base no-args constructor" __ e..
+//   ? in. x  value x  c.. __ "Base (int) overloaded constructor" __ e..
+//   #? c.. __ "Base destructor" __ e..
+//
+//
+//c_ Derived  pu.. B..
+//    u.. B.. B..
+//pr..
+//    in. doubled_value
+//pu..
+//    ?  d.. 0  c.. __ "Derived no-args constructor " __ e..
+//    ? in. x  d.. x*2   c.. __ "Derived (int) overloaded constructor" __ e..
+//    #? c.. __ "Derived destructor " __ e..
+//
+//
+//in. main
+////   Base b;
+////    Base b{100};
+// //   Derived d;
+// ? d 1000
+//
+//    r_ _
