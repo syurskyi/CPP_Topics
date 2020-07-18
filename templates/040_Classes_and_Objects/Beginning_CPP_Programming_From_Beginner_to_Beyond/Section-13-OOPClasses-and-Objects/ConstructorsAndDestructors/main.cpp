c6214550cf7ece082e61582bc5651cafd1b92af8
Grey <sergejyurskyj@yahoo.com>
2020-07-18 12:17:37 -0700

    Committed CopyConstructor template

--- templates/040_Classes_and_Objects/Beginning_CPP_Programming_From_Beginner_to_Beyond/Section-13-OOPClasses-and-Objects/ConstructorsAndDestructors/main.cpp
+++ templates/040_Classes_and_Objects/Beginning_CPP_Programming_From_Beginner_to_Beyond/Section-13-OOPClasses-and-Objects/ConstructorsAndDestructors/main.cpp
@@ -1,61 +1,61 @@
-// Section 13
-// Constructors and Destructors
-
-? ios..
-? st..
-
-u.. s..
-
-c_ Player
-
-pr..
-   st. st..  name
-   in. health
-   in. xp
-pu..
-    v.. set_name st. st..  name_val
-        name _ name_val; 
-    
-    // Overloaded Constructors
-    ?
-        c.. __ "No args constructor called"__ e..
-    
-    ? st. st..  name
-        c.. __ "String arg constructor called"__ e..
-    
-    ? st. st..  name, in. health, in. xp
-        c.. __ "Three args constructor called"__ e..
-
-     #?
-        c.. __ "Destructor called for " __ name __ e..
-    
-
-
-in. main
-
-    
-        ? slayer
-        ?.s_n.. "Slayer"
-    
-    
-    
-        ? frank
-        ?.s_n.. "Frank"
-        ? hero "Hero"
-        ?.s_n.. "Hero"
-        ? villain "Villain", 100, 12
-        ?.s.. "Villain"
-    
-    
-    ? #enemy _ ne. ?
-    ?__s_n.. "Enemy"
-    
-    ? #level_boss _ ne. ? "Level Boss", 1000, 300
-    ?__s_n.. "Level Boss"
-    
-    de... ?
-    de... ?
-    
-    r_ _
-
-
+//// Section 13
+//// Constructors and Destructors
+//
+//? ios..
+//? st..
+//
+//u.. s..
+//
+//c_ Player
+//
+//pr..
+//   st. st..  name
+//   in. health
+//   in. xp
+//pu..
+//    v.. set_name st. st..  name_val
+//        name _ name_val;
+//
+//    // Overloaded Constructors
+//    ?
+//        c.. __ "No args constructor called"__ e..
+//
+//    ? st. st..  name
+//        c.. __ "String arg constructor called"__ e..
+//
+//    ? st. st..  name, in. health, in. xp
+//        c.. __ "Three args constructor called"__ e..
+//
+//     #?
+//        c.. __ "Destructor called for " __ name __ e..
+//
+//
+//
+//in. main
+//
+//
+//        ? slayer
+//        ?.s_n.. "Slayer"
+//
+//
+//
+//        ? frank
+//        ?.s_n.. "Frank"
+//        ? hero "Hero"
+//        ?.s_n.. "Hero"
+//        ? villain "Villain", 100, 12
+//        ?.s.. "Villain"
+//
+//
+//    ? #enemy _ ne. ?
+//    ?__s_n.. "Enemy"
+//
+//    ? #level_boss _ ne. ? "Level Boss", 1000, 300
+//    ?__s_n.. "Level Boss"
+//
+//    de... ?
+//    de... ?
+//
+//    r_ _
+//
+//
--- templates/040_Classes_and_Objects/Beginning_CPP_Programming_From_Beginner_to_Beyond/Section-13-OOPClasses-and-Objects/CopyConstructor/main.cpp
+++ templates/040_Classes_and_Objects/Beginning_CPP_Programming_From_Beginner_to_Beyond/Section-13-OOPClasses-and-Objects/CopyConstructor/main.cpp
@@ -1,54 +1,54 @@
-// Section 13
-// Copy Constructor
-? ios..
-? st..
-
-u.. s..
-
-c_ Player
-{
-pr..
-   st. st..  name;
-   in. health;
-   in. xp;
-pu..
-    st. st..  get_name r_ name; }
-    in. get_health r_ health; }
-    in. get_xpreturn xp; }
-    Player(st. st..  name_val ="None", in. health_val = 0, in. xp_val = 0);
-    // Copy constructor
-    Player(co.. Player &source);
-    // Destructor
-    ~Player c.. __ "Destructor called for: " __ name __ e.. }
-};
-
-Player::Player(st. st..  name_val, in. health_val, in. xp_val)
-    : name{name_val}, health{health_val}, xp{xp_val} {
-            c.. __ "Three-args constructor for " + name __ e..
-}
-
-Player::Player(co.. Player &source)
-   // : name(source.name), health(source.health), xp{source.xp} {
-       : Player {source.name, source.health, source.xp}  {
-        c.. __ "Copy constructor - made copy of: " __ source.name __ e..
-}
-
-v.. display_player(Player p) {
-    c.. __ "Name: " __ p.get_name() __ e..
-    c.. __ "Health: " __ p.get_health() __ e..
-    c.. __ "XP: " __ p.get_xp() __ e..
-}
-
-in. main
-    Player empty {"XXXXXX", 100, 50};
-    
-    Player my_new_object {empty};
-    
-    display_player(empty);
-    
-    Player frank {"Frank"};
-    Player hero {"Hero", 100};
-    Player villain {"Villain", 100, 55};
-        
-    r_ 0;
-}
+//// Section 13
+//// Copy Constructor
+//? ios..
+//? st..
+//
+//u.. s..
+//
+//c_ Player
+//
+//pr..
+//   st. st..  name
+//   in. health
+//   in. xp
+//pu..
+//    st. st..  get_name r_ name
+//    in. get_health r_ health
+//    in. get_xpreturn xp
+//    ?(st. st..  name_val ="None", in. health_val = 0, in. xp_val = 0);
+//    // Copy constructor
+//    ?(co.. ? #source
+//    // Destructor
+//    #? c.. __ "Destructor called for: " __ n.. __ e..
+//
+//
+//? ? st. st..  name_val, in. health_val, in. xp_val
+//    : n.. ? h.. ? x. ?
+//            c.. __ "Three-args constructor for " + n.. __ e..
+//
+//
+//? ? co.. ? #source
+//   // : name(source.name), health(source.health), xp source.xp
+//       | ?  ?.n.. ?.h.. ?.x.
+//        c.. __ "Copy constructor - made copy of: " __ ?.n.. __ e..
+//
+//
+//v.. display_player ? p
+//    c.. __ "Name: " __ ?.g_n.. __ e..
+//    c.. __ "Health: " __ ?.g_h.. __ e..
+//    c.. __ "XP: " __ ?.g_x. __ e..
+//
+//
+//in. main
+//    ? empty  "XXXXXX", 100, 50
+//
+//    ? my_new_object  empty
+//
+//    d_p.. e..
+//
+//    ? frank  "Frank"
+//    ? hero  "Hero", 100
+//    ? villain  "Villain", 100, 55
+//
+//    r_ _
+//
