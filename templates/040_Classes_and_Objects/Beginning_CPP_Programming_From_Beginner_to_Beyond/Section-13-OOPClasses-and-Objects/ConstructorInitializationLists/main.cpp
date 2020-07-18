1a564e3e48d07c5d1cfb1de02febd6d7b9c88c9f
Grey <sergejyurskyj@yahoo.com>
2020-07-18 08:22:40 -0700

    Committed ConstructorInitializationLists template

--- templates/040_Classes_and_Objects/Beginning_CPP_Programming_From_Beginner_to_Beyond/Section-13-OOPClasses-and-Objects/ConstructorInitializationLists/main.cpp
+++ templates/040_Classes_and_Objects/Beginning_CPP_Programming_From_Beginner_to_Beyond/Section-13-OOPClasses-and-Objects/ConstructorInitializationLists/main.cpp
@@ -1,42 +1,41 @@
-// Section 13
-// Constructor Initialization Lists
-? ios..
-? st..
-
-u.. s..
-
-c_ Player
-{
-pr..
-   st. st..  name {"XXXXXXX"};
-   in. health;
-   in. xp;
-pu..
-// Overloaded Constructors
-    Player();
-    Player(st. st..  name_val);
-    Player(st. st..  name_val, in. health_val, in. xp_val);
-};
-
-Player::Player() 
-    : name{"None"}, health{0}, xp{0} {
-}
-
-Player::Player(st. st..  name_val)
-   : name{name_val}, health{0}, xp{0} {
-}
-  
-Player::Player(st. st..  name_val, in. health_val, in. xp_val)
-    : name{name_val}, health{health_val}, xp{xp_val} {
-    
-}
-
-in. main
-    
-    Player empty;
-    Player frank {"Frank"};
-    Player villain {"Villain", 100, 55};
-    
-    r_ 0;
-}
-
+//// Section 13
+//// Constructor Initialization Lists
+//? ios..
+//? st..
+//
+//u.. s..
+//
+//c_ Player
+//
+//pr..
+//   st. st..  name "XXXXXXX"
+//   in. health
+//   in. xp
+//pu..
+//// Overloaded Constructors
+//    ?
+//    ? st. st..  name_val
+//    ? st. st..  name_val, in. health_val, in. xp_val
+//
+//
+//? ?
+//    | n.. "None" h.. 0 x. 0
+//
+//
+//? ? st. st..  name_val
+//   | n.. ? h.. 0 x. 0
+//
+//
+//? ? st. st..  name_val, in. health_val, in. xp_val
+//    | n.. ? h.. ? x. ?
+//
+//
+//in. main
+//
+//    ? empty
+//    ? frank "Frank"
+//    ? villain "Villain", 100, 55
+//
+//    r_ _
+//
+//
