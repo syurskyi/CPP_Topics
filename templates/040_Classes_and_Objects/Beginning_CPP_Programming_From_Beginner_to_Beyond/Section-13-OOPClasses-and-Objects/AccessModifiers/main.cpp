5beed6ec8dee584ce6119f46cd99ea0759b56210
Grey <sergejyurskyj@yahoo.com>
2020-07-18 08:12:37 -0700

    Committed AccessModifiers template

--- templates/040_Classes_and_Objects/Beginning_CPP_Programming_From_Beginner_to_Beyond/Section-13-OOPClasses-and-Objects/AccessModifiers/main.cpp
+++ templates/040_Classes_and_Objects/Beginning_CPP_Programming_From_Beginner_to_Beyond/Section-13-OOPClasses-and-Objects/AccessModifiers/main.cpp
@@ -1,27 +1,27 @@
-// Section 13
-// Access Modifers
-? ios..
-? st..
-
-u.. s..
-
-c_ Player {
-pr..
-    // attributes
-    st..  name {"Player"};
-    in. health;
-    in. xp;
-pu..
-    // methods
-    v.. talk(st..  text_to_say) { c.. __ name __ " says " __ text_to_say __ e..  }
-    b.. is_dead();
-};
-
-in. main
-    Player frank;
-  //  frank.name = "Frank";
-  //  cout << frank.health << endl;
-  frank.talk("Hello there");
-    r_ 0;
-}
-
+//// Section 13
+//// Access Modifers
+//? ios..
+//? st..
+//
+//u.. s..
+//
+//c_ Player
+//pr..
+//    // attributes
+//    st..  name "Player"
+//    in. health
+//    in. xp
+//pu..
+//    // methods
+//    v.. talk st..  text_to_say  c.. __ ? __ " says " __ text_to_say __ e..
+//    b.. is_dead
+//
+//
+//in. main
+//    ? frank
+//  //  frank.name = "Frank";
+//  //  cout << frank.health << endl;
+//  ?.t.. "Hello there"
+//    r_ _
+//
+//
