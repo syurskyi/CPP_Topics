2d858bbdbdc512ebb16fe96198d8b6e709143441
Grey <sergejyurskyj@yahoo.com>
2020-07-19 11:16:51 -0700

    Committed enum template

--- templates/OTUS_Developer_CPP/04_Comparison_of_CPP11_CPP14_with_CPP03/enum.cpp
+++ templates/OTUS_Developer_CPP/04_Comparison_of_CPP11_CPP14_with_CPP03/enum.cpp
@@ -1,34 +1,34 @@
-? |i..
-
-en.. Alphabet 
-    a, b, c
-}sy.. p..
-
-en.. c_ Digit : in. 
-    one, two, three
-}sy.. p..
-
-v.. accept_in.(in.) 
-}
-
-v.. accept_class_enum(Digit) 
-}
-
-v.. accept_enum(Alphabet) 
-}
-
-in. main(in., c.. #||
-    accept_in.(0)sy.. p..
-    // accept_class_enum(0); // no known conversion from 'int' to 'Digit'
-    // accept_enum(0); // no known conversion from 'int' to 'Alphabet'
-
-    accept_in.(a)sy.. p..
-//    accept_class_enum(b); // no known conversion from 'Alphabet' to 'Digit'
-    accept_enum(c)sy.. p..
-
-//    accept_int(Digit::one); // no known conversion from 'Digit' to 'int'
-    accept_class_enum(Digit::two)sy.. p..
-//    accept_enum(Digit::three); // no known conversion from 'Digit' to 'Alphabet'
-
-    r_ 0sy.. p..
-}
+//? |i..
+//
+//en.. Alphabet
+//    a, b, c
+//sy.. p..
+//
+//en.. c_ Digit  in.
+//    one, two, three
+//sy.. p..
+//
+//v.. accept_in. in.
+//
+//
+//v.. accept_class_enum D..
+//
+//
+//v.. accept_enum A..
+//
+//
+//in. main in., c.. #||
+//    a_in.. 0 sy.. p..
+//    // accept_class_enum(0); // no known conversion from 'int' to 'Digit'
+//    // accept_enum(0); // no known conversion from 'int' to 'Alphabet'
+//
+//    a_i... a sy.. p..
+////    accept_class_enum(b); // no known conversion from 'Alphabet' to 'Digit'
+//    a_e.. c sy.. p..
+//
+////    accept_int(Digit::one); // no known conversion from 'Digit' to 'int'
+//    a_c_e.. D.. two sy.. p..
+////    accept_enum(Digit::three); // no known conversion from 'Digit' to 'Alphabet'
+//
+//    r_ _ sy.. p..
+//
