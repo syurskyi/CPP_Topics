a0889c2cc139d043ec92a250388fc8c9b90ccf87
Grey <sergejyurskyj@yahoo.com>
2020-07-19 11:55:55 -0700

    Committed 020 C-SOURCE-CODE-for-Loops-exercises-nested-loops template

--- templates/CPP_From_Beginner_to_Expert/06 Loops/020 C-SOURCE-CODE-for-Loops-exercises-nested-loops/Untitled1.cpp
+++ templates/CPP_From_Beginner_to_Expert/06 Loops/020 C-SOURCE-CODE-for-Loops-exercises-nested-loops/Untitled1.cpp
@@ -1,45 +1,40 @@
-? |i..
-
-u.. s..
-/* loops - exercises and nested loops */
-
-main()
-{
-   /* int nr = 1234; // 4 digits
-    int nrOfDigits = 1;
-
-    cout << 1234 / 10 << endl;
-    cout << 123 / 10 << endl;
-    cout << 12 / 10 << endl;
-    cout << 1 / 10 << endl;
-
-    int tmp = nr;
-    while (tmp /= 10) //tmp = 0, nrOfDigits = 4
-        nrOfDigits++;
-
-    cout << "the number " << nr << " has " << nrOfDigits << " digits" << endl;
-*/
-    /*
-        1 2 3 4 5  6  7  8  9  10
-        2 4 6 8 10 12 14 16 18 20
-        3 6 9 12 15 ....
-        4 ...
-        5 ...
-    */
-
-    ___ (in. i _ 1sy.. p.. i <_ 15sy.. p.. ###) // i = 2
-    {
-        ___ (in. j _ 1sy.. p.. j <_ 12sy.. p.. j++) // j = 1
-        {
-            c__.width(4)sy.. p..
-            c__ __  i * jsy.. p..
-        }
-
-
-        c__ __  e..
-    }
-
-
-
-
-}
+//? |i..
+//
+//u.. s..
+///* loops - exercises and nested loops */
+//
+//main
+//
+//   /* int nr = 1234; // 4 digits
+//    int nrOfDigits = 1;
+//
+//    cout << 1234 / 10 << endl;
+//    cout << 123 / 10 << endl;
+//    cout << 12 / 10 << endl;
+//    cout << 1 / 10 << endl;
+//
+//    int tmp = nr;
+//    while (tmp /= 10) //tmp = 0, nrOfDigits = 4
+//        nrOfDigits++;
+//
+//    cout << "the number " << nr << " has " << nrOfDigits << " digits" << endl;
+//*/
+//    /*
+//        1 2 3 4 5  6  7  8  9  10
+//        2 4 6 8 10 12 14 16 18 20
+//        3 6 9 12 15 ....
+//        4 ...
+//        5 ...
+//    */
+//
+//    ___ in. ? _ 1 sy.. p.. ? <_ 15 sy.. p.. ### // i = 2
+//
+//        ___ in. j _ 1 sy.. p.. ? <_ 12 sy.. p.. ### // j = 1
+//
+//            c__.wi.. 4 sy.. p..
+//            c__ __  ? * ? sy.. p..
+//
+//
+//
+//        c__ __  e..
+//
