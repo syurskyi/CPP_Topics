0e142deb6f4fc37308e70d7150255e8cb506fc2d
Grey <sergejyurskyj@yahoo.com>
2020-07-19 11:26:39 -0700

    Committed move template

--- templates/OTUS_Developer_CPP/04_Comparison_of_CPP11_CPP14_with_CPP03/move.cpp
+++ templates/OTUS_Developer_CPP/04_Comparison_of_CPP11_CPP14_with_CPP03/move.cpp
@@ -1,64 +1,64 @@
-? |i..
-
-c_ Big 
-    c..* buffersy.. p..
-pu..
-    Big() : buffer(n.. c..[1000]) 
-        st. c__ __  __P.. __  " // allocate 1G" __  st. e..
-    }
-    Big(c.. Big &other) : buffer(n.. c..[1000]) 
-        st. c__ __  __P.. __  " // duplicate 1G" __  st. e..
-        st. memcpy(buffer, other.buffer, 1000)sy.. p..
-    }
-    Big(Big &&other) : buffer(other.buffer) 
-        st. c__ __  __P.. __  " // steal 1G" __  st. e..
-    }
-    ~Big
-        st. c__ __  __P.. __  " // destroy 1G" __  st. e..
-        de..[] buffersy.. p..
-    }
-    Big& operator_(c.. Big &other) 
-        st. c__ __  __P.. __  " // duplicate 1G" __  st. e..
-        st. memcpy(buffer, other.buffer, 1000)sy.. p..
-        r_ *thissy.. p..
-    }
-    Big& operator_(Big &&other) 
-        st. c__ __  __P.. __  " // steal 1G" __  st. e..
-        buffer _ other.buffersy.. p..
-        r_ *thissy.. p..
-    }
-}sy.. p..
-
-v.. bar(c.. Big &) 
-    st. c__ __  __P.. __  st. e..
-}
-
-v.. bar(Big &&) 
-    st. c__ __  __P.. __  st. e..
-}
-
-t..<t_n_ T>
-v.. foo(T &&t) 
-    st. c__ __  __P.. __  st. e..
-    bar(st. forward<T>(t))sy.. p..
-}
-
-in. main(in., c.. #||
-    Big big, big2sy.. p..
-
-    st. c__ __  st. e..
-    big _ big2sy.. p..
-
-    st. c__ __  st. e..
-    big _ Big()sy.. p..
-
-    st. c__ __  st. e..
-    big _ st. move(big2)sy.. p..
-
-    st. c__ __  "}" __  st. e..
-
-//    foo(big);
-//    foo(Big());
-
-    r_ 0sy.. p..
-}
+//? |i..
+//
+//c_ Big
+//    c..# buffer sy.. p..
+//pu..
+//    ?  b.. n.. c..|1000
+//        st. c__ __  __P.. __  " // allocate 1G" __  st. e..
+//
+//    ? c.. ? #other  b.. n.. c..|1000
+//        st. c__ __  __P.. __  " // duplicate 1G" __  st. e..
+//        st. m_sp_ b.. o__.b.. 1000 sy.. p..
+//
+//    ? ? ##other  b.. o__.b..
+//        st. c__ __  __P.. __  " // steal 1G" __  st. e..
+//
+//    #?
+//        st. c__ __  __P.. __  " // destroy 1G" __  st. e..
+//        de..|| b.. y.. p..
+//
+//    ?# operator_ c.. ? #o..
+//        st. c__ __  __P.. __  " // duplicate 1G" __  st. e..
+//        st. m_sp_ b.. o__.b.. 1000 sy.. p..
+//        r_ #t.. sy.. p..
+//
+//    ?# operator_ ? ##o..
+//        st. c__ __  __P.. __  " // steal 1G" __  st. e..
+//        buffer _ o__.b.. sy.. p..
+//        r_ #t.. sy.. p..
+//
+//sy.. p..
+//
+//v.. bar c.. ? #
+//    st. c__ __  __P.. __  st. e..
+//
+//
+//v.. bar ? ##
+//    st. c__ __  __P.. __  st. e..
+//
+//
+//t..|t_n_ ?|
+//v.. foo ? ##t)
+//    st. c__ __  __P.. __  st. e..
+//    bar st. forward|?| t sy.. p..
+//
+//
+//in. main in., c.. #||
+//    ? ?, big2 sy.. p..
+//
+//    st. c__ __  st. e..
+//    ? _ _2 sy.. p..
+//
+//    st. c__ __  st. e..
+//    ? _ ? sy.. p..
+//
+//    st. c__ __  st. e..
+//    ? _ st. move _2 sy.. p..
+//
+//    st. c__ __  "}" __  st. e..
+//
+////    foo(big);
+////    foo(Big());
+//
+//    r_ 0 sy.. p..
+//
