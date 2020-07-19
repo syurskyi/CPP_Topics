8877b56ab42bfa3dbb0e6d30d234b2a07ddea0e8
Grey <sergejyurskyj@yahoo.com>
2020-07-19 11:33:54 -0700

    Committed 03.range template

--- templates/OTUS_Developer_CPP/05_CPPOverview_17_Constexpr_lambda_Fold_expression_Attributes_Type_deduction/03.range/step0.cpp
+++ templates/OTUS_Developer_CPP/05_CPPOverview_17_Constexpr_lambda_Fold_expression_Attributes_Type_deduction/03.range/step0.cpp
@@ -1,35 +1,35 @@
-? |i..
-? |s..
-
-t..<t_n_ Input>
-c_ filter_helper  
-    c.. Input &inputsy.. p..
-
-    c_ iterator  
-        t_n_ Input::const_iterator currentsy.. p..
-    pu..
-        ex__ iterator(t_n_ Input::const_iterator current_) : current(current_)  }sy.. p..
-
-        a.. op..!_(t_n_ Input::const_iterator rhs) c..   r_ current !_ rhssy.. p.. }sy.. p..
-
-        a.. op..++ r_ ++currentsy.. p.. }sy.. p..
-
-        a.. &op..*() c..   r_ *currentsy.. p.. }sy.. p..
-    }sy.. p..
-
-
-pu..
-    ex__ filter_helper(c.. Input &input_) : input(input_)  }
-
-    a.. begin() c..   r_ iterator st. cbegin(input)}sy.. p.. }
-
-    a.. end() c..   r_ st. cend(input)sy.. p.. }
-}sy.. p..
-
-in. main
-    c.. st. s.. s "hello"}sy.. p..
-
-    ___ (a.. i: filter_helper s})  
-        st. c__ __  i __  st. e..
-    }
-}
+//? |i..
+//? |s..
+//
+//t..|t_n_ Input|
+//c_ filter_helper
+//    c.. Input #input sy.. p..
+//
+//    c_ iterator
+//        t_n_ In.. const_iterator current sy.. p..
+//    pu..
+//        ex__ it.. t_n_ I.. c_i.. current_|  cu.. cu.._  sy.. p..
+//
+//        a.. op..!_|t_n_ I.. c_i.. rhs| c..   r_ cu.. !_ r.. sy.. p.. |sy.. p..
+//
+//        a.. op..## r_ ##cu.. sy.. p.. |sy.. p..
+//
+//        a.. #op..#|| c..   r_ #cu.. sy.. p.. |sy.. p..
+//    sy.. p..
+//
+//
+//pu..
+//    ex__ filter_helper|c.. I.. #i.._  ? ?_
+//
+//    a.. begin c..   r_ it.. st. cbegin i.. sy.. p.. }
+//
+//    a.. end c..   r_ st. cend i.. sy.. p..
+//sy.. p..
+//
+//in. main
+//    c.. st. s.. s "hello" sy.. p..
+//
+//    ___ a.. i| filter_helper s
+//        st. c__ __  ? __  st. e..
+//
+//
