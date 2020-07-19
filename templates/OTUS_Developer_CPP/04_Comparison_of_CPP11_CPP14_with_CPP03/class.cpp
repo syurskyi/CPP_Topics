d99e840b606ce5709add16a435eb112dee0ebb3e
Grey <sergejyurskyj@yahoo.com>
2020-07-19 11:06:30 -0700

    Committed class template

--- templates/OTUS_Developer_CPP/04_Comparison_of_CPP11_CPP14_with_CPP03/class.cpp
+++ templates/OTUS_Developer_CPP/04_Comparison_of_CPP11_CPP14_with_CPP03/class.cpp
@@ -1,26 +1,25 @@
-? |i..
-? ve..
-
-st.. foo
-
-    foo() _ deletesy.. p..
-    v.. ~foo() _ defaultsy.. p..
-
-    v.. v.. m() final }sy.. p..
-    v.. v.. n}sy.. p..
-}sy.. p..
-
-c_ bar final : foo
-
-   // void m() override {}; // overrides a 'final'
-    v.. n() ov.. }sy.. p..
-   v.. o() ov.. }sy.. p.. // only virtual member functions can be marked 'override'
-}sy.. p..
-
-// class baz : bar
-// {
-// }
-
-in. main(in., c.. #||
-    r_ 0sy.. p..
-}
+//? |i..
+//? ve..
+//
+//st.. foo
+//
+//    ? _ de.. sy.. p..
+//    v.. #? _ def.. sy.. p..
+//
+//    v.. v.. m() final sy.. p..
+//    v.. v.. n sy.. p..
+//sy.. p..
+//
+//c_ bar final  f..
+//
+//   // void m() override {}; // overrides a 'final'
+//    v.. n ov.. sy.. p..
+//   v.. o ov.. sy.. p.. // only virtual member functions can be marked 'override'}sy.. p..
+//
+//// class baz : bar
+//// {
+//// }
+//
+//in. main in., c.. #||
+//    r_ _ sy.. p..
+//
