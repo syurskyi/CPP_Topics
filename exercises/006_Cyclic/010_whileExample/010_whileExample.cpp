e48041086ac8921d5e2cf998a723ca9278bd77a4
Grey <sergejyurskyj@yahoo.com>
2020-07-19 14:22:26 -0700

    Committed 010_whileExample exercise

--- exercises/006_Cyclic/010_whileExample/010_whileExample.cpp
+++ exercises/006_Cyclic/010_whileExample/010_whileExample.cpp
@@ -1,40 +1,41 @@
-﻿//? *s..
-//? |i..
-//? |s..
-//
-//u.. s..
-//
-//in. main
-//
-//
-//	setlocale(LC_CTYPE, "rus")
-//
-//	s.. str _ "Guess the desired color from 5 attempts."
-//
-//	in. maxAttempt _ 5;	// Допустимое количество попыток
-//	in. attempt _ 0		// Номер текущей попытки
-//	s.. color _ "red"	// Задуманый цвет
-//
-//	w___ a.. < m..
-//		a##
-//		c__ __  s.. __  " Attempt " __  a.. __  e..
-//		s.. value _ ""
-//		c__ __ ?
-//
-//		__ ? __ ""
-//
-//			b..
-//
-//		__ v.. !_ c..
-//
-//			str _ "You did not guess"
-//			c___
-//
-//
-//		c__ __  "Congratulations, you guessed right with " __  a.. __  " attempts!" __  e..
-//		b..
-//	}
-//
-//	sy.. pause
-//	r_ _
-//
+﻿#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+
+{
+	setlocale(LC_CTYPE, "rus");
+
+	string str = "Guess the desired color from 5 attempts.";
+
+	int maxAttempt = 5;	// Допустимое количество попыток
+	int attempt = 0;		// Номер текущей попытки
+	string color = "red";	// Задуманый цвет
+
+	while (attempt < maxAttempt){
+		attempt++;
+		cout <<  str <<  " Attempt " <<  attempt <<  endl;
+		string value = "";
+		cin >> value;
+
+		if (value == "")
+		{
+            break;
+        }
+		if (value != color)
+		{
+            str = "You did not guess";
+            continue;
+        }
+
+		cout <<  "Congratulations, you guessed right with " <<  attempt <<  " attempts!" << endl;
+		break;
+	}
+
+	system("pause");
+	return 0;
+}
+
