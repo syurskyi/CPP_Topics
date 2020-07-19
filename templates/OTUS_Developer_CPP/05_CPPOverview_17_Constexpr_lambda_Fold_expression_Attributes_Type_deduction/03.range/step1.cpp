874b467e5a9060a3ba263dc9eab4bd59fee12188
Grey <sergejyurskyj@yahoo.com>
2020-07-19 11:40:03 -0700

    Committed 03.range template

--- templates/OTUS_Developer_CPP/05_CPPOverview_17_Constexpr_lambda_Fold_expression_Attributes_Type_deduction/03.range/step1.cpp
+++ templates/OTUS_Developer_CPP/05_CPPOverview_17_Constexpr_lambda_Fold_expression_Attributes_Type_deduction/03.range/step1.cpp
@@ -1,37 +1,37 @@
-? |i..
-? |s..
-
-t..<t_n_ Input, t_n_ UnaryPredicate> // 2. UnaryPredicate
-c_ filter_helper { // 3. wrapper => filter
-    c.. Input &inputsy.. p..
-    c.. UnaryPredicate &psy.. p.. // 4. p
-
-    c_ iterator {
-        c.. filter_helper &ownersy.. p.. // 5.
-        t_n_ Input::const_iterator currentsy.. p..
-    pu..
-        ex__ iterator(c.. filter_helper &owner_) : owner(owner_), current(st. cbegin(owner_.input)) {}sy.. p..
-
-        a.. op..!_(t_n_ Input::const_iterator rhs) c.. { r_ current !_ rhssy.. p.. }sy.. p..
-
-        a.. op..++ r_ ++currentsy.. p.. }sy.. p..
-
-        a.. op..*() c.. { r_ owner.p(*current)sy.. p.. }sy.. p..
-    }sy.. p..
-
-
-pu..
-    ex__ filter_helper(c.. Input &input_, c.. UnaryPredicate &p_) : input(input_), p(p_) {}
-
-    a.. begin() c.. { r_ iterator{*t..}sy.. p.. }
-
-    a.. end() c.. { r_ st. cend(input)sy.. p.. }
-}sy.. p..
-
-in. main
-    c.. st. s.. s{"hello"}sy.. p..
-
-    ___ (a.. i: filter_helper{s, [](a.. c) { r_ c __ 'l'sy.. p.. }}) { // 1. predicate
-        st. c__ __  i __  st. e..
-    }
-}
+//? |i..
+//? |s..
+//
+//t..|t_n_ Input, t_n_ UnaryPredicate| // 2. UnaryPredicate
+//c_ filter_helper  // 3. wrapper => filter
+//    c.. I.. #i.. sy.. p..
+//    c.. U.. #p sy.. p.. // 4. p
+//
+//    c_ iterator
+//        c.. filter_helper #owner sy.. p.. // 5.
+//        t_n_ I.. const_iterator current sy.. p..
+//    pu..
+//        ex__ it.. c.. f_h.. #o.._)  ? ?_| cu.. st. cbegin ?_.i.. || sy.. p..
+//
+//        a.. op..!_|t_n_ I.. c_i.. rh_| c.. | r_ cu.. !_ rh_ sy.. p.. |sy.. p..
+//
+//        a.. op..## r_ ##cu.. sy.. p.. |sy.. p..
+//
+//        a.. op..#|| c.. | r_ o__.p|#cu..|sy.. p.. |sy.. p..
+//    sy.. p..
+//
+//
+//pu..
+//    ex__ f_h..|c.. I.. #i.._, c.. U.. #p_|  i.. i.._| p|?_ ||
+//
+//    a.. begin c.. | r_ it..|#t..| sy.. p..
+//
+//    a.. end c.. | r_ st. cend|i.. sy.. p..
+//sy.. p..
+//
+//in. main
+//    c.. st. s.. s "hello" sy.. p..
+//
+//    ___ (a.. i| f_h..|s, |||a.. c || r_ c __ 'l' sy.. p..  // 1. predicate
+//        st. c__ __  i __  st. e..
+//
+//
