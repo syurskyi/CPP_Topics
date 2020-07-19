324661f27cb2165db6e48d46af123310237b8cc8
Grey <sergejyurskyj@yahoo.com>
2020-07-19 11:15:08 -0700

    Committed ctor template

--- templates/OTUS_Developer_CPP/04_Comparison_of_CPP11_CPP14_with_CPP03/ctor.cpp
+++ templates/OTUS_Developer_CPP/04_Comparison_of_CPP11_CPP14_with_CPP03/ctor.cpp
@@ -1,17 +1,16 @@
-? |i..
-
-st.. foo 
-    foo st. c__ __  __P.. __  st. e.. }sy.. p..
-
-    foo(in.) : foo  st. c__ __  __P.. __  st. e.. }sy.. p..
-
-    foo(c.. c.. *) : foo0}  st. c__ __  __P.. __  st. e.. }sy.. p..
-}sy.. p..
-
-in. main(in., c.. #||
-    foo f42}sy.. p..
-
-    foo g"42"}sy.. p..
-
-    r_ 0sy.. p..
-}
+//? |i..
+//
+//st.. foo
+//    ? st. c__ __  __P.. __  st. e.. sy.. p..
+//
+//    ? in.|  ?  st. c__ __  __P.. __  st. e..  sy.. p..
+//
+//    ?|c.. c.. # | ?0  st. c__ __  __P.. __  st. e.. sy.. p..
+//sy.. p..
+//
+//in. main(in., c.. #||
+//    f.. f 42 sy.. p..
+//
+//    f.. g "42" sy.. p..
+//
+//    r_ _ sy.. p..
