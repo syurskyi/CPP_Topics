7a3ec983d4a482152c973587710f5c40b6b5994e
Grey <sergejyurskyj@yahoo.com>
2020-07-18 17:06:30 -0700

    Committed TheProblem template

---
+++ templates/040_Classes_and_Objects/Beginning_CPP_Programming_From_Beginner_to_Beyond/Section-16-Polymorphism/TheProblem/main.cpp
@@ -0,0 +1,47 @@
+//// Section 16
+//// The problem
+//? ios..
+//? me..
+//
+//c_ Base
+//pu..
+//    v.. say_hello co..
+//        st. c.. __ "Hello - I'm a Base class object" __ st. e..
+//
+//
+//
+//c_ Derived pu.. ?
+//pu..
+//    v.. say_hello  co..
+//        st. c.. __ "Hello - I'm a Derived class object" __ st. e..
+//
+//
+//
+//v.. greetings co.. Base #obj
+//    st. c.. __ "Greetings: "
+//    o__.s..
+//
+//
+//in. main
+//    B.. b
+//    ?.s..
+//
+//    ? d
+//    ?.s..
+//
+//    g.. b
+//    g.. d
+//
+//    Base #ptr _ ne. D..
+//    ptr__s..
+//
+//    st. u..|B..| ptr1 _ st. m..<D..
+//    ?__s..
+//
+//
+//
+//   de... p..
+//
+//    r_ _
+//
+//
