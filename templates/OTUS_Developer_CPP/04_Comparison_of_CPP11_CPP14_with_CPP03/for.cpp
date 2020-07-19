81ef02b8ef5fbaa8edfb527a65c7d0422a9950a6
Grey <sergejyurskyj@yahoo.com>
2020-07-19 11:19:12 -0700

    Committed for template

--- templates/OTUS_Developer_CPP/04_Comparison_of_CPP11_CPP14_with_CPP03/for.cpp
+++ templates/OTUS_Developer_CPP/04_Comparison_of_CPP11_CPP14_with_CPP03/for.cpp
@@ -1,38 +1,38 @@
-? <array>
-? |i..
-? <map>
-? ve..
-
-
-in. main(in., c.. #||
-    in. a[10]sy.. p..
-    ___(a.. i: a)
-    
-        st. c__ __  i __  st. e..
-    }
-
-    st. ve..<in.> vsy.. p..
-    
-        a.. begin _ v.begin()sy.. p..
-        a.. end _ v.end()sy.. p..
-        ___(sy.. p.. begin !_ endsy.. p.. ++begin)
-        
-            in. i _ *beginsy.. p..
-            st. c__ __  i __  st. e..
-        }
-    }
-
-    st. s.. s"hello"}sy.. p..
-    ___(a.. c: s)
-    
-        st. c__ __  c __  st. e..
-    }
-
-    st. map<in., st. s..> m{{1, "1"}}sy.. p..
-    ___(a.. c: m)
-    
-//        std::cout << c << std::endl;
-    }
-
-    r_ 0sy.. p..
-}
+//? <array>
+//? |i..
+//? <map>
+//? ve..
+//
+//
+//in. main(in., c.. #||
+//    in. a 10 sy.. p..
+//    ___ a.. i| a
+//
+//        st. c__ __  ? __  st. e..
+//
+//
+//    st. ve..|in.| v sy.. p..
+//
+//        a.. begin _ ?.b.. sy.. p..
+//        a.. end _ ?.e.. sy.. p..
+//        ___(sy.. p.. b.. !_ e.. sy.. p.. ##b..
+//
+//            in. i _ #b.. sy.. p..
+//            st. c__ __  ? __  st. e..
+//
+//
+//
+//    st. s.. s"hello" sy.. p..
+//    ___ a.. c| s
+//
+//        st. c__ __  ? __  st. e..
+//
+//
+//    st. map|in., st. s..| m||1, "1"|| sy.. p..
+//    ___ a.. c| m
+//
+////        std::cout << c << std::endl;
+//
+//
+//    r_ _ sy.. p..
+//
