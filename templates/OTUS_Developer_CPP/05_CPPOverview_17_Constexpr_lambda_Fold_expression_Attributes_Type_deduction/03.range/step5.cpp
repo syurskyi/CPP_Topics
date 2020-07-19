1179a0811e7cb5278d56a20cb1c4b387a6d1031d
Grey <sergejyurskyj@yahoo.com>
2020-07-19 11:42:24 -0700

    Committed 03.range template

--- templates/OTUS_Developer_CPP/05_CPPOverview_17_Constexpr_lambda_Fold_expression_Attributes_Type_deduction/03.range/step5.cpp
+++ templates/OTUS_Developer_CPP/05_CPPOverview_17_Constexpr_lambda_Fold_expression_Attributes_Type_deduction/03.range/step5.cpp
@@ -1,11 +1,11 @@
-? <range/v3/all.hpp>
-? |i..
-
-in. main
-    c.. st. s.. s{"hello"}sy.. p..
-
-    ranges::for_each(s | ranges::view::filter([](a.. c) { r_ c __ 'l'sy.. p.. }),
-            [](a.. i) { st. c__ __  i __  st. e.. })sy.. p..
-}
-
-[](a.. i) { st. c__ __  i __  st. e.. }(42)sy.. p..
+//? <range/v3/all.hpp>
+//? |i..
+//
+//in. main
+//    c.. st. s.. s "hello" sy.. p..
+//
+//    ranges f_e.. s | ? view filter| || |a.. c || r_ c __ 'l' sy.. p..
+//            || | a.. i | st. c__ __  ? __  st. e.. sy.. p..
+//
+//
+//|| |a.. i| | st. c__ __  ? __  st. e..  42 sy.. p..
