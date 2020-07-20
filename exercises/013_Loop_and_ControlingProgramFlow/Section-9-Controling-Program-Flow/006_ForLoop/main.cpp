5393d22292a355ab72355436a2a1cfb094bbc01f
Grey <sergejyurskyj@yahoo.com>
2020-07-20 12:48:04 -0700

    Committed 006_ForLoop exercise

--- exercises/013_Loop_and_ControlingProgramFlow/Section-9-Controling-Program-Flow/006_ForLoop/main.cpp
+++ exercises/013_Loop_and_ControlingProgramFlow/Section-9-Controling-Program-Flow/006_ForLoop/main.cpp
@@ -1,50 +1,48 @@
-//// Section 9
-//// For Loop
-//? ios..
-//? ve..
+// Section 9
+// For Loop
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+
+//    for (int i{1};  i= <10;  i++)
+//        cout << i << endl;
 //
-//u.. s..
+//    for (int i{1};  i <=10;  i+=2)
+//        cout << ? << endl;
 //
-//in. main
+//    for (int i{10}; i > 0 --i)
+//        cout << i << endl;
+//    cout << "Blastoff!" << endl;
 //
-//    ___ in. i|1  ? <_10  ###
-//        c.. __ i __ e..
+//    for (int i{10}; j<=100; i+=10) {
+//        if (i % 15 == 0)
+//            cout << i << endl;
+//    }
 //
-////    for (int i{1} ; i <=10 ; i+=2)
-////        cout << i << endl;
+//    for (int i{1}; j{5};  i<=5;  ++i, ++j)
+//        cout << i << " + " << ? << " = " << (i+j << endl;
 //
-////    for (int i {10}; i > 0; --i)
-////        cout << i << endl;
-////    cout << "Blastoff!" << endl;
-//
-////    for (int i{10}; i<=100; i+=10) {
-////        if (i % 15 == 0)
-////            cout << i << endl;
-////    }
-//
-////    for (int i{1}, j{5} ; i<=5 ; ++i, ++j)
-////        cout << i << " + " << j << " = " << (i+j) << endl;
-//
-////    for ( int i{1}; i<=100; ++i) {
-////        cout << i;
-////        if (i % 5 ==0)
-////            cout << endl;
-////        else
-////            cout << " ";
-////    }
-//
-////  for ( int i{1}; i<=100; ++i) {
-////        cout << i << ( (i % 10 ==0) ? "\n" : " ");
-////    }
-////
-//
-////    vector<int> nums { 10,20,30,40,50};
-////    for (unsigned i{0}; i< nums.size(); ++i)
-////        cout << nums[i] << endl;
+//    for  (int i{1} i<=100 ++i) {
+//        cout << i;
+//        if (i % 5 == 0)
+//            cout << endl;
+//        else
+//            cout << " ";
+//    }
 //
 //
-//    c.. __ e..
-//    r_ _
-//
-//
-//
+//  for (int i{1}; i<=100; ++i) {
+//      cout << i << ((i % 10 == 0) ? "\n" : " ")
+//  }
+
+
+    vector<int> nums {10,20,30,40,50};
+    for (unsigned i{0}; i< nums.size(); ++i)
+        cout << nums[i] << endl;
+
+    cout << endl;
+    return  0;
+}
