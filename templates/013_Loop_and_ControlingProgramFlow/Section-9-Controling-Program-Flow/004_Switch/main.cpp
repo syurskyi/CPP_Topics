df263410c9e65180f543814527214f325afb9ab1
Grey <sergejyurskyj@yahoo.com>
2020-07-20 11:41:20 -0700

    Committed 004_Switch exercise

--- templates/013_Loop_and_ControlingProgramFlow/Section-9-Controling-Program-Flow/004_Switch/main.cpp
+++ templates/013_Loop_and_ControlingProgramFlow/Section-9-Controling-Program-Flow/004_Switch/main.cpp
@@ -12,37 +12,37 @@
 //
 //in. main
 //
-//    ch.. letter_grade
+//    ch.. letter_grade||
 //
-//    c.. __ "Enter the letter grade you expect on the exam: ";
+//    c.. __ "Enter the letter grade you expect on the exam: "
 //    ci. __ ?
 //
 //    sw.. ?
-//        ca.. 'a'
-//        ca.. 'A'
+//        c.. 'a'
+//        c.. 'A'
 //            c.. __ "You need a 90 or above, study hard!" __ e..
 //            b..
-//        ca.. 'b'
-//        ca.. 'B'
+//        c.. 'b'
+//        c.. 'B'
 //            c.. __ "You need 80-89 for a B, go study!" __ e..
 //            b..
-//        ca.. 'c'
-//        ca.. 'C'
+//        c.. 'c'
+//        c.. 'C'
 //            c.. __ "You need 70-79 for an average grade" __ e..
 //            b..
-//        ca.. 'd'
-//        ca.. 'D'
+//        c.. 'd'
+//        c.. 'D'
 //            c.. __ "Hmm, you should strive for a better grade. All you need is 60-69" __ e..
 //            b..
-//        ca.. 'f'
-//        ca.. 'F'
+//        c.. 'f'
+//        c.. 'F'
 //
 //            ch.. confirm
 //            c.. __ "Are you sure (Y/N)? "
 //            ci. __ ?
-//            __ (? __ 'y' __ ? __ 'Y')
+//            __ ? __ 'y' __ ? __ 'Y'    // Logic operation  or
 //                c.. __ "OK, I guess you didn't study..." __ e..
-//            ____ __ (? __ 'n' __ ? __ 'N')
+//            ____ __ ? __ 'n' __ ? __ 'N'
 //                c.. __ "Good- go study!" __ e..
 //            ____
 //                c.. __ "Illegal choice" __ e..
