fd02c180faf6a042bf77ea3237af834d33342ce1
Grey <sergejyurskyj@yahoo.com>
2020-07-20 10:52:32 -0700

    Committed 001_IfStatement exercise

---
+++ exercises/013_Loop_and_ControlingProgramFlow/Section-9-Controling-Program-Flow/008_WhileLoop/main.cpp
@@ -0,0 +1,58 @@
+//// Section 9
+//// While Loop
+//? ios..
+//
+//u.. s..
+//
+//in. main
+//
+//    in. num
+//    c.. __ "Enter a positive integer - start the countdown :";
+//    ci. __ ?
+//
+//    w__ ? > 0
+//        c.. __ ? __ e..
+//       ##?
+//
+//    c.. __ "Blastoff!" __ e..
+//
+////    int num{};
+////    cout << "Enter a postive integer to count up to: ";
+////    cin >> num;
+////
+////    int i {1};
+////    while (num >= i) {
+////        cout << i << endl;
+////        i++;
+////    }
+//
+////    int number {};
+////
+////    cout << "Enter an integer less than 100: ";
+////    cin >> number;
+////
+////    while (number >= 100) {  // !(number < 100)
+////        cout << "Enter an integer less than 100: ";
+////        cin >> number;
+////    }
+////
+////    cout << "Thanks" << endl;
+//
+////    bool done {false};
+////    int number {0};
+////
+////    while (!done) {
+////           cout << "Enter an integer between 1 and 5: ";
+////           cin >> number;
+////           if (number <=1 || number >=5)
+////               cout << "Out of range, try again" << endl;
+////           else {
+////               cout << "Thanks!" << endl;
+////               done = true;
+////            }
+////       }
+//
+//    c..  __ e..
+//    r_ _
+//
+//
