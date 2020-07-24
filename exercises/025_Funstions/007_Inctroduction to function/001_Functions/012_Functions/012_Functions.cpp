ea263830631c913fc04f09e150dc92b81d696e09
Grey <sergejyurskyj@yahoo.com>
2020-07-23 19:48:42 -0700

    Committed 012_Functions exercise

--- exercises/025_Funstions/007_Inctroduction to function/001_Functions/012_Functions/012_Functions.cpp
+++ exercises/025_Funstions/007_Inctroduction to function/001_Functions/012_Functions/012_Functions.cpp
@@ -1,35 +1,36 @@
-//? *s..
-//? |i..
-//? |s..
-//
-//u.. s..
-//
-//v.. S..
-//v.. G..
-//
-//s.. value _ "Global variable!"
-//
-//in. main
-//
-//	s.. value _ "Local variable in Main!"
-//
-//	c__ __  ? __  e..
-//
-//	?
-//
-//	?
-//
-//	c__.g..
-//    r_ _
-//
-//
-//v.. SampleFunction
-//
-//	s.. value _ "Local variable in SampleFunction!"
-//
-//	c__ __  ? __  e..
-//
-//
-//v.. Global
-//
-//	c__ __  value __  e..
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void SampleFunction();
+void Global();
+
+string value = "Global variable!";
+
+int main() {
+
+	string value = "Local variable in Main!";
+
+	cout <<  value <<  endl;
+
+	SampleFunction();
+
+	Global();
+
+	cin.get();
+    return 0;
+
+
+void SampleFunction() {
+
+	string value = "Local variable in SampleFunction!";
+
+	cout <<  value <<  endl;
+}
+
+void Global() {
+
+	cout <<  value <<  endl;
+}
