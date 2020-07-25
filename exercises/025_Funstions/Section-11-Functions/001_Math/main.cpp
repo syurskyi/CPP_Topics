949e7705d82adc19fe642dd6d3e403882344a63c
Grey <sergejyurskyj@yahoo.com>
2020-07-24 21:30:18 -0700

    Committed Section-11-Functions exercise

--- exercises/025_Funstions/Section-11-Functions/001_Math/main.cpp
+++ exercises/025_Funstions/Section-11-Functions/001_Math/main.cpp
@@ -1,35 +1,35 @@
-// Section 11
-// Math examples
-
-? ios..
-? <cmath>        // required
-
-u.. s..
-
-in. main
-
-    do.. num
-
-    c.. __ "Enter a number (double) : "
-    ci. __ ?
-
-    c.. __ "The sqrt of " __ ? __ " is: " __ sq.. ? __ e..
-    c.. __ "The cubed root of " __ ? __ " is: " __ cb.. ? __ e..
-
-    c.. __ "The sine of " __ ? __ " is: " __ si. ? __ e..
-    c.. __ "The cosine of " __ ? __ " is: " __ co. ? __ e..
-
-    c.. __ "The ceil of " __ ? __ " is: " __ ce.. ? __ e..
-    c.. __ "The floor of " __ ? __ " is: " __ fl.. ? __ e..
-    c.. __ "The round of " __ ? __ " is: " __ ro.. ? __ e..
-
-
-    do.. power
-    c.. __ "\nEnter a power to raise " __ ? __ " to: "
-    ci. __ ?
-    c.. __ ? __ " raised to the " __ ? __ " power is: " __ po. ?, ? __ e..
-
-
-    c.. __  e..
-    r_ _
-
+//// Section 11
+//// Math examples
+//
+//? ios..
+//? <cmath>        // required
+//
+//u.. s..
+//
+//in. main
+//
+//    do.. num
+//
+//    c.. __ "Enter a number (double) : "
+//    ci. __ ?
+//
+//    c.. __ "The sqrt of " __ ? __ " is: " __ sq.. ? __ e..
+//    c.. __ "The cubed root of " __ ? __ " is: " __ cb.. ? __ e..
+//
+//    c.. __ "The sine of " __ ? __ " is: " __ si. ? __ e..
+//    c.. __ "The cosine of " __ ? __ " is: " __ co. ? __ e..
+//
+//    c.. __ "The ceil of " __ ? __ " is: " __ ce.. ? __ e..
+//    c.. __ "The floor of " __ ? __ " is: " __ fl.. ? __ e..
+//    c.. __ "The round of " __ ? __ " is: " __ ro.. ? __ e..
+//
+//
+//    do.. power
+//    c.. __ "\nEnter a power to raise " __ ? __ " to: "
+//    ci. __ ?
+//    c.. __ ? __ " raised to the " __ ? __ " power is: " __ po. ?, ? __ e..
+//
+//
+//    c.. __  e..
+//    r_ _
+//
--- exercises/025_Funstions/Section-11-Functions/003_FunctionDefinitions/main.cpp
+++ exercises/025_Funstions/Section-11-Functions/003_FunctionDefinitions/main.cpp
@@ -1,44 +1,44 @@
-// Section 11
-// Function Definitions
-// Area of Circle and Volume of a Cylinder
-? ios..
-
-u.. s..
-
-co.. do.. pi 3.14159
-
-do.. calc_area_circle do.. radius
-    r_ p. * r.. * r..
-
-
-do.. calc_volume_cylinder do.. radius, do.. height
-//    return pi * radius * radius * height;
-    r_ c_a_c..(r.. * h..
-
-
-v.. area_circle
-    do.. r..
-    c.. __ "\nEnter the radius of the circle: "
-    ci. __ ?
-    c.. __ "The area of a circle with radius " __ ? __ " is " __ c_a_c.. ? __ e..
-
-
-v.. volume_cylinder
-    do.. r..
-    do.. h..
-    c.. __ "\nEnter the radius of the cylinder: "
-    ci. __ ?
-    c.. __ "\nEnter the height of the cylinder: "
-    ci. __ ?
-
-    c.. __ "The volume of a cylinder with radius " __ ? __ " and height " __ h.. __ " is " __ c_v_c.. r.. h.. __ e..
-
-
-in. main
-
-    a..
-    a..
-    v..
-
-    r_ _
-
+//// Section 11
+//// Function Definitions
+//// Area of Circle and Volume of a Cylinder
+//? ios..
+//
+//u.. s..
+//
+//co.. do.. pi 3.14159
+//
+//do.. calc_area_circle do.. radius
+//    r_ p. * r.. * r..
+//
+//
+//do.. calc_volume_cylinder do.. radius, do.. height
+////    return pi * radius * radius * height;
+//    r_ c_a_c..(r.. * h..
+//
+//
+//v.. area_circle
+//    do.. r..
+//    c.. __ "\nEnter the radius of the circle: "
+//    ci. __ ?
+//    c.. __ "The area of a circle with radius " __ ? __ " is " __ c_a_c.. ? __ e..
+//
+//
+//v.. volume_cylinder
+//    do.. r..
+//    do.. h..
+//    c.. __ "\nEnter the radius of the cylinder: "
+//    ci. __ ?
+//    c.. __ "\nEnter the height of the cylinder: "
+//    ci. __ ?
+//
+//    c.. __ "The volume of a cylinder with radius " __ ? __ " and height " __ h.. __ " is " __ c_v_c.. r.. h.. __ e..
+//
+//
+//in. main
+//
+//    a..
+//    a..
+//    v..
+//
+//    r_ _
+//
