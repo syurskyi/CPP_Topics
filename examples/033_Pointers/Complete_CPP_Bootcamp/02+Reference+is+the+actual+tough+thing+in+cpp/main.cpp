39ab72a2832571f09450506b9c1d4f9f927d7b90
Grey <sergejyurskyj@yahoo.com>
2020-09-01 15:33:01 -0700

    Committed Complete_CPP_Bootcamp examples

---
+++ examples/033_Pointers/Complete_CPP_Bootcamp/02+Reference+is+the+actual+tough+thing+in+cpp/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  get_started
+//
+//  Created by syurskyi on 01/09/20.
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int score = 200;
+    int *myp = &score;
+    
+    printf("Value of score is %d\n", score);
+    printf("Value of pointer is %p\n", myp);
+    
+    int &another_score = score;
+    another_score = 800;
+    
+    printf("Value of score is %d\n", score);
+    printf("Value of pointer is %p\n", myp);
+    
+    return 0;
+}
---
+++ examples/033_Pointers/Complete_CPP_Bootcamp/03+Cpp+array+are+different+with+pointers/main.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  get_started
+//
+//  Created by syurskyi on 01/09/20.
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int integer_array[4] = {1, 2, 3, 4};
+    cout << integer_array[0] << endl;
+    
+    
+    int another_array[4];
+    another_array[0] = 9;
+    another_array[1] = 19;
+    
+    cout << another_array[0] << endl;
+    
+    *another_array = 29;
+    cout << another_array[0] << endl;
+    
+    
+    cout << another_array[1] << endl;
+    int *ap = another_array;
+    ap++;
+    *ap = 209;
+    cout << another_array[1] << endl;
+    
+    
+    
+    return 0;
+}
